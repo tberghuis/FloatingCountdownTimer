f928bbd675d59857fce866fbdf39f395e545b73c
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-07-09 20:06:20 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/stopwatch/StopwatchStateHolder.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/stopwatch/StopwatchStateHolder.kt
@@ -1,5 +1,12 @@
 package xyz.tberghuis.floatingtimer.stopwatch
 
+// make sure use like
+// state.willitblend
+
+// find all StopwatchStateHolder import
+// replace with import StopwatchStateHolder as state
+// delete all import StopwatchStateHolder.*
+
 object StopwatchStateHolder {
   val willitblend = "willitblend"
 }
