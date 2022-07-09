513657797c3f59f52af2615acd3d252dbf504cb3
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-07-09 19:57:37 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/stopwatch/StopwatchStateHolder.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/stopwatch/StopwatchStateHolder.kt
@@ -1,6 +1,5 @@
 package xyz.tberghuis.floatingtimer.stopwatch
 
 object StopwatchStateHolder {
-
   val willitblend = "willitblend"
 }
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/stopwatch/servicecomposables.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/stopwatch/servicecomposables.kt
@@ -6,14 +6,14 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import xyz.tberghuis.floatingtimer.logd
-import xyz.tberghuis.floatingtimer.stopwatch.StopwatchStateHolder.willitblend
+import xyz.tberghuis.floatingtimer.stopwatch.StopwatchStateHolder as state
 
 @Composable
 fun StopwatchServiceOverlay(exit: () -> Unit) {
 
   Column(verticalArrangement = Arrangement.Center) {
     Text("hello stopwatch overlay")
-    Text(willitblend)
+    Text(state.willitblend)
     Button(onClick = {
       logd("exit composable")
       exit()
