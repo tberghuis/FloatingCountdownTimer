a825acac0d663f05431fc6e0992124e95d9f5b48
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-07-11 10:50:37 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/stopwatch/composables/StopwatchClickTarget.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/stopwatch/composables/StopwatchClickTarget.kt
@@ -1,11 +1,13 @@
 package xyz.tberghuis.floatingtimer.stopwatch.composables
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import xyz.tberghuis.floatingtimer.logd
 
 
 @Composable
@@ -13,8 +15,18 @@ fun StopwatchClickTarget() {
   Box(
     modifier = Modifier
       .background(Color.Red)
+      .clickable {
+        onClickStopwatchClickTarget()
+      }
+
   ) {
     Text("hello click target")
   }
 
+}
+
+
+// todo move into another file
+fun onClickStopwatchClickTarget(){
+  logd("click target")
 }
