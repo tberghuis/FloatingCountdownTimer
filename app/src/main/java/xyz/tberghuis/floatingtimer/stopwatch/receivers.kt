37913069abbbf351ffbe95c68549b04bafc86784
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-07-12 13:08:38 +1000

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/stopwatch/receivers.kt
@@ -0,0 +1,20 @@
+package xyz.tberghuis.floatingtimer.stopwatch
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import xyz.tberghuis.floatingtimer.INTENT_COMMAND
+import xyz.tberghuis.floatingtimer.INTENT_COMMAND_EXIT
+import xyz.tberghuis.floatingtimer.logd
+
+
+class StopwatchExitReceiver : BroadcastReceiver() {
+  override fun onReceive(context: Context, intent: Intent) {
+    logd("StopwatchExitReceiver onReceive")
+
+    val exitIntent = Intent(context.applicationContext, StopwatchService::class.java)
+    exitIntent.putExtra(INTENT_COMMAND, INTENT_COMMAND_EXIT)
+    context.startForegroundService(exitIntent)
+  }
+}
+
