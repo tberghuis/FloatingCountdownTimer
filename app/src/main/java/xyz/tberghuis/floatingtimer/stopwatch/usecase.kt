f8545b945837c7535c431a15bcf9df5909fb1928
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-10-13 11:28:32 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/stopwatch/usecase.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/stopwatch/usecase.kt
@@ -3,25 +3,13 @@ package xyz.tberghuis.floatingtimer.stopwatch
 import android.app.Service
 import xyz.tberghuis.floatingtimer.logd
 
-// doitwrong
 // todo move to a usecase class, inject with hilt/dagger
 fun stopwatchExit(stopwatchOverlayComponent: StopwatchOverlayComponent) {
   logd("stopwatchExit")
   // fix, this makes timertask cancel
   stopwatchState.resetStopwatchState()
-
   stopwatchOverlayComponent.removeOverlays()
-
   // todo, here decides to stopForground if no overlays
-//  stopwatchServiceHolder.exitStopwatch()
-
-
-  // doitwrong
+  //  stopwatchServiceHolder.exitStopwatch()
   (stopwatchOverlayComponent.context as Service).stopSelf()
 }
-
-
-//class StopwatchExit {
-//
-//}
-
