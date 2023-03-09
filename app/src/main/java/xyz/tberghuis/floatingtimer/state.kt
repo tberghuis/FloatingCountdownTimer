3ff915e7eec389e7d14ac9b88dd0a16facaef083
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-03-10 07:15:28 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/state.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/state.kt
@@ -6,35 +6,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlinx.coroutines.flow.MutableStateFlow
 
-// TODO refactor to class
-//object OverlayStateHolder {
-//  var durationSeconds: Int = 10
-//  var countdownSeconds by mutableStateOf<Int>(10)
-//
-//  val timerState = MutableStateFlow<TimerState>(TimerStatePaused)
-//
-//  // don't really need because i can cancel with requestCode???
-//  var pendingAlarm: PendingIntent? = null
-//}
-
-
 class OverlayStateHolder {
   var durationSeconds: Int = 10
   var countdownSeconds by mutableStateOf<Int>(10)
-
   val timerState = MutableStateFlow<TimerState>(TimerStatePaused)
 
   // don't really need because i can cancel with requestCode???
   var pendingAlarm: PendingIntent? = null
-
 
   fun resetTimerState(duration: Int) {
     durationSeconds = duration
     countdownSeconds = duration
     timerState.value = TimerStatePaused
   }
-
-
 }
 
 
