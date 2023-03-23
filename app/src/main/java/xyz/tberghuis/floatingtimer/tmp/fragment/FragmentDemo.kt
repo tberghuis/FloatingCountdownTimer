9aa4b05aea1fca9eb143a85b4f9196b4f771eb08
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-03-24 06:12:09 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/fragment/FragmentDemo.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/fragment/FragmentDemo.kt
@@ -1,19 +1,27 @@
 package xyz.tberghuis.floatingtimer.tmp.fragment
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun FragmentDemo() {
+  val context = LocalContext.current
   Column {
-    Button(onClick = {}) {
+    Button(onClick = {
+      showOverlay(context)
+    }) {
       Text("show overlay")
     }
   }
 }
 
-fun showOverlay(){
-
+fun showOverlay(context: Context){
+  val intent = Intent(context.applicationContext, FragmentService::class.java)
+  context.startForegroundService(intent)
 }
+
