fef0657d7940f76755a2abfad370ca9224c4cdec
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-06-29 09:09:03 +1000

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/IapDemo.kt
@@ -0,0 +1,42 @@
+package xyz.tberghuis.floatingtimer.tmp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
+
+
+class IapDemo : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setContent {
+      FloatingTimerTheme {
+        Surface(
+          modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        ) {
+          IapDemoScreen()
+        }
+      }
+    }
+
+
+  }
+}
+
+@Composable
+fun IapDemoScreen() {
+  Column {
+    Text("dsad")
+  }
+}
