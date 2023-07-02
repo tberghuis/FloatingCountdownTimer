3820a07e32eb216298654a29798be6ba728ef045
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-02 13:32:00 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/colorpicker/ColorPickerScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/colorpicker/ColorPickerScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,6 +21,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.github.skydoves.colorpicker.compose.AlphaSlider
 import com.github.skydoves.colorpicker.compose.AlphaTile
 import com.github.skydoves.colorpicker.compose.BrightnessSlider
+import com.github.skydoves.colorpicker.compose.ColorPickerController
 import com.github.skydoves.colorpicker.compose.HsvColorPicker
 import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 
@@ -27,52 +29,60 @@ import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 fun ColorPickerScreen(
   vm: ColorPickerScreenVm = viewModel()
 ) {
-    val controller = rememberColorPickerController()
+  val controller = rememberColorPickerController()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(all = 30.dp)
+//  val controller = remember {
+//    ColorPickerController().apply {
+//      this.selectByCoordinate()
+//    }
+//  }
+
+
+  Column(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(all = 30.dp)
+  ) {
+    Row(
+      modifier = Modifier.fillMaxWidth(),
+      horizontalArrangement = Arrangement.Center,
+      verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            AlphaTile(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(60.dp)
-                    .clip(RoundedCornerShape(6.dp)),
-                controller = controller
-            )
-        }
-        HsvColorPicker(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(450.dp)
-                .padding(10.dp),
-            controller = controller,
-            onColorChanged = {
-                Log.d("Color", it.hexCode)
-            }
-        )
-        AlphaSlider(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-                .height(35.dp),
-            controller = controller,
-            tileOddColor = Color.White,
-            tileEvenColor = Color.Black
-        )
-        BrightnessSlider(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-                .height(35.dp),
-            controller = controller,
-        )
+      AlphaTile(
+        modifier = Modifier
+          .fillMaxWidth()
+          .height(60.dp)
+          .clip(RoundedCornerShape(6.dp)),
+        controller = controller
+      )
     }
+    HsvColorPicker(
+      modifier = Modifier
+        .fillMaxWidth()
+        .height(450.dp)
+        .padding(10.dp),
+      controller = controller,
+      onColorChanged = {
+        Log.d("Color", it.hexCode)
+      },
+      initialColor = Color.Blue
+    )
+    AlphaSlider(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp)
+        .height(35.dp),
+      controller = controller,
+      tileOddColor = Color.White,
+      tileEvenColor = Color.Black
+    )
+    BrightnessSlider(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp)
+        .height(35.dp),
+      controller = controller,
+    )
+  }
 
 }
