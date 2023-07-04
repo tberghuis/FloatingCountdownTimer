a213efbf58cc09539e3394ccacce2c4d0c6f03d3
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-04 10:39:40 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/colorpicker/ColorPickerScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/colorpicker/ColorPickerScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -59,6 +61,7 @@ fun ColorPickerScreen(
     modifier = Modifier
       .fillMaxSize()
       .padding(all = 30.dp)
+//      .verticalScroll(rememberScrollState())
   ) {
     Row(
       modifier = Modifier.fillMaxWidth(),
