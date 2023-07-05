6381e0315b7e3030cfa48a7efef34afd10160c36
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-06 06:02:02 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/MainActivity.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/MainActivity.kt
@@ -5,41 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
-import xyz.tberghuis.floatingtimer.data.PreferencesRepository
 import xyz.tberghuis.floatingtimer.screens.HomeScreen
-import xyz.tberghuis.floatingtimer.tmp.VibrationDemo
 import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
-
-
-
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     logd("onCreate")
-
-
     setContent {
-
-
       FloatingTimerTheme {
-
         Surface(
           modifier = Modifier
             .fillMaxSize()
@@ -47,7 +26,6 @@ class MainActivity : ComponentActivity() {
         ) {
           HomeScreen()
         }
-
       }
     }
   }
