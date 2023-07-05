5139f23fc95c4bd9ffc0d69c3c8b504abc2a6761
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-06 06:13:10 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/MainActivity.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/MainActivity.kt
@@ -6,8 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.tberghuis.floatingtimer.screens.HomeScreen
 import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
@@ -24,8 +31,30 @@ class MainActivity : ComponentActivity() {
             .fillMaxSize()
             .background(Color.White),
         ) {
-          HomeScreen()
+          FloatingTimerNavHost()
         }
+      }
+    }
+  }
+}
+
+
+val LocalNavController = compositionLocalOf<NavHostController> {
+  error("CompositionLocal LocalNavController not present")
+}
+
+@Composable
+fun FloatingTimerNavHost(
+) {
+  val navController = rememberNavController()
+  CompositionLocalProvider(LocalNavController provides navController) {
+    NavHost(
+      navController = navController, startDestination = "home"
+    ) {
+      composable("home") {
+        HomeScreen()
+      }
+      composable("settings") {
       }
     }
   }
