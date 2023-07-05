fbb07bfc1bb88398bff62e23e8483a9cefebf02c
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-06 07:16:30 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/screens/SettingsScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/screens/SettingsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -37,18 +38,33 @@ fun SettingsScreen() {
         modifier = Modifier
           .fillMaxWidth()
           .clickable {
-          logd("change timer color")
-          // if not purchased
-          // show dialog
-          // else
-          // nav halo_colour_change
-        },
+            logd("change timer color")
+            // if not purchased
+            // show dialog
+            // else
+            // nav halo_colour_change
+          },
       ) {
         Text("Change Timer color")
         Text("LOCKED ICON")
       }
     }
   }
+
+
+  AlertDialog(
+    onDismissRequest = {},
+    confirmButton = {},
+    modifier = Modifier,
+    dismissButton = {},
+    title = { Text("Premium Feature") },
+    text = {
+      Column {
+        Text("Purchase premium feature: Change Timer Halo Color?")
+        Text("todo list price")
+      }
+    },
+  )
 
 
 }
