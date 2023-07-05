0861bb6644cfb8e56c4070254e958107ba047233
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-06 06:43:50 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/screens/SettingsScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/screens/SettingsScreen.kt
@@ -1,12 +1,34 @@
 package xyz.tberghuis.floatingtimer.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen() {
-  Column {
-    Text("settings")
+
+  Scaffold(
+    modifier = Modifier,
+    topBar = {
+      TopAppBar(
+        title = { Text("Settings") },
+        modifier = Modifier,
+      )
+    },
+    snackbarHost = {},
+  ) { padding ->
+    Column(
+      modifier = Modifier.padding(padding),
+    ) {
+      Text("settings")
+    }
   }
+
+
 }
