f236e030334252b30c81d7c1db9acdad11df05b5
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-06 07:04:26 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/screens/SettingsScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/screens/SettingsScreen.kt
@@ -1,13 +1,19 @@
 package xyz.tberghuis.floatingtimer.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import xyz.tberghuis.floatingtimer.logd
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,6 +33,20 @@ fun SettingsScreen() {
       modifier = Modifier.padding(padding),
     ) {
       Text("settings")
+      Row(
+        modifier = Modifier
+          .fillMaxWidth()
+          .clickable {
+          logd("change timer color")
+          // if not purchased
+          // show dialog
+          // else
+          // nav halo_colour_change
+        },
+      ) {
+        Text("Change Timer color")
+        Text("LOCKED ICON")
+      }
     }
   }
 
