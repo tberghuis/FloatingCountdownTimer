f8ad7a7a69df257597af9c563983a4159b50456a
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-29 12:02:30 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/maccascomposables.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/maccascomposables.kt
@@ -1,13 +1,16 @@
 package xyz.tberghuis.floatingtimer.tmp
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 
 @Composable
 fun MaccasBubble() {
-  Box {
+  Box(modifier = Modifier.background(Color.Gray)) {
     Text("0:00")
   }
 }
