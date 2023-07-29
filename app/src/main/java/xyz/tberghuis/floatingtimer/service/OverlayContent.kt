2700564dc6962608218a2a20122d4c6796fcdb82
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-30 08:21:55 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/service/OverlayContent.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/service/OverlayContent.kt
@@ -1,11 +1,7 @@
 package xyz.tberghuis.floatingtimer.service
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
-import xyz.tberghuis.floatingtimer.logd
 import xyz.tberghuis.floatingtimer.service.countdown.CountdownOverlay
 import xyz.tberghuis.floatingtimer.service.stopwatch.StopwatchOverlay
 
@@ -15,19 +11,10 @@ fun OverlayContent() {
   val isCountdownVisible = state.countdownState.overlayState.isVisible.collectAsState()
   val isStopwatchVisible = state.stopwatchState.overlayState.isVisible.collectAsState()
 
-  Box(Modifier.onGloballyPositioned {
-//    state.screenWidthPx = it.size.width
-//    state.screenHeightPx = it.size.height
-
-//    logd("OverlayContent onGloballyPositioned ${state.screenWidthPx} ${state.screenHeightPx}")
-
-  }) {
-    // future.txt correct z-order
-    if (isCountdownVisible.value == true) {
-      CountdownOverlay(state)
-    }
-    if (isStopwatchVisible.value == true) {
-      StopwatchOverlay()
-    }
+  if (isCountdownVisible.value == true) {
+    CountdownOverlay(state)
+  }
+  if (isStopwatchVisible.value == true) {
+    StopwatchOverlay()
   }
 }
