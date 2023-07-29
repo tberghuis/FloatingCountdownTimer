ccc07a389ccdc4e89c418d58c4996c5586dc3d8a
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-29 10:45:16 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/MaccasScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/MaccasScreen.kt
@@ -1,19 +1,31 @@
 package xyz.tberghuis.floatingtimer.tmp
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import xyz.tberghuis.floatingtimer.logd
 
 
 @Composable
 fun MaccasScreen() {
+  val context = LocalContext.current
+
   Column {
     Button(onClick = {
-      logd("button")
+      logd("startMaccasService")
+      startMaccasService(context)
     }) {
-      Text("button")
+      Text("startMaccasService")
     }
   }
 }
+
+fun startMaccasService(context: Context) {
+  val intent = Intent(context.applicationContext, MaccasService::class.java)
+  context.startForegroundService(intent)
+}
+
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/MaccasService.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/MaccasService.kt
@@ -3,9 +3,16 @@ package xyz.tberghuis.floatingtimer.tmp
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import xyz.tberghuis.floatingtimer.logd
 
 class MaccasService: Service() {
   override fun onBind(intent: Intent?): IBinder? {
     return null
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+
+    logd("MaccasService oncreate")
   }
 }
