6a4b8e7bab76b3a7162cdc704e9dcfffa3280cfb
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-07-29 13:06:31 +1000

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/fullscreen.kt
@@ -0,0 +1,17 @@
+package xyz.tberghuis.floatingtimer.tmp
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun MaccasFullscreen() {
+  Box(
+    modifier = Modifier.background(Color.Gray)
+  ) {
+    Text("0:00")
+  }
+}
