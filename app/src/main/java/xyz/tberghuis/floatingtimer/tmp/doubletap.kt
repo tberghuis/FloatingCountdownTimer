7e2f3336ef07a93a60bddf7c5f74c1a64d05282d
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-08-01 12:33:56 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/doubletap.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/doubletap.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import xyz.tberghuis.floatingtimer.tmp.dragdrop.DDScreen
 import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
 
 class DoubleTapActivity : ComponentActivity() {
@@ -22,7 +23,8 @@ class DoubleTapActivity : ComponentActivity() {
             .fillMaxSize()
             .background(Color.White),
         ) {
-          MaccasScreen()
+//          MaccasScreen()
+          DDScreen()
         }
       }
     }
