550062aaa8d5c4966e3c24c6d1d7ea9d0ea66c16
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-08-02 07:06:55 +1000

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/service/stopwatch/IsDraggingOverlay.kt
@@ -0,0 +1,27 @@
+package xyz.tberghuis.floatingtimer.service.stopwatch
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import xyz.tberghuis.floatingtimer.composables.Trash
+import xyz.tberghuis.floatingtimer.service.LocalServiceState
+
+@Composable
+fun IsDraggingOverlay() {
+  val serviceState = LocalServiceState.current
+  val stopwatchState = serviceState.stopwatchState
+  val overlayState = stopwatchState.overlayState
+
+
+
+  Column(
+    Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Bottom,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Trash(overlayState)
+  }
+}
