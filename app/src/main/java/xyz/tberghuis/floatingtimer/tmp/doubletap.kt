33d4cb6896e6b062fcdc05205022e20136c38f60
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-08-01 12:29:37 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/doubletap.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/doubletap.kt
@@ -4,27 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.unit.dp
-import xyz.tberghuis.floatingtimer.logd
 import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
 
-
 class DoubleTapActivity : ComponentActivity() {
-
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -43,36 +29,3 @@ class DoubleTapActivity : ComponentActivity() {
   }
 }
 
-@Composable
-fun DoubleTapDemo() {
-  Column(
-    verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally,
-  )
-  {
-    Box(
-      modifier = Modifier
-
-        .pointerInput(Unit) {
-          detectTapGestures(
-            onDoubleTap = {
-              logd("onDoubleTap")
-            },
-            onTap = {
-              logd("onTap")
-            }
-          )
-        }
-
-        .clickable {
-          logd("clickable")
-        }
-
-
-        .border(1.dp, Color.Black),
-    ) {
-      Text("double tap")
-
-    }
-  }
-}
