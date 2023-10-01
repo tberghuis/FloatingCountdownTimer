1a0d55b0b58420676c1d628d5d9b5aa165393d85
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-10-01 13:43:48 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpOverlayController.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpOverlayController.kt
@@ -1,12 +1,16 @@
 package xyz.tberghuis.floatingtimer.tmp
 
+import android.content.Context
+import android.graphics.PixelFormat
 import android.view.View
+import android.view.WindowManager
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.ui.platform.ComposeView
 import androidx.lifecycle.setViewTreeLifecycleOwner
 import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import xyz.tberghuis.floatingtimer.TIMER_SIZE_PX
 import xyz.tberghuis.floatingtimer.logd
 
 class TmpOverlayController(val service: TmpService) {
@@ -31,5 +35,21 @@ class TmpOverlayController(val service: TmpService) {
         }
       }
     }
+
+    reallyAddComposeView()
+  }
+
+  private fun reallyAddComposeView() {
+    val windowManager = service.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    val params = WindowManager.LayoutParams(
+      TIMER_SIZE_PX,
+      TIMER_SIZE_PX,
+      0,
+      0,
+      WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+      WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+      PixelFormat.TRANSLUCENT
+    )
+    windowManager.addView(contentView!!, params)
   }
 }
