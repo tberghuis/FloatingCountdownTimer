439a01dd55dddd5312c012c31391103866c9da71
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-10-01 15:18:10 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/OverlayViewController.kt
+++
@@ -1,9 +0,0 @@
-package xyz.tberghuis.floatingtimer.tmp2
-
-import android.util.Log
-import android.view.WindowManager
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
-
