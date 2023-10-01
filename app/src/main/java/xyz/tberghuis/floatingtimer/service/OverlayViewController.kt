4ffd66c770d5fc58b2c59ce44ff5d00ad23355e1
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-10-01 15:17:39 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/service/OverlayViewController.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/service/OverlayViewController.kt
@@ -6,4 +6,42 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+import xyz.tberghuis.floatingtimer.tmp2.OverlayViewHolder
 
+class OverlayViewController(
+  val createOverlayViewHolder: () -> OverlayViewHolder,
+  val isVisible: Flow<Boolean?>,
+  val windowManager: WindowManager,
+  scope: CoroutineScope
+) {
+  // doitwrong
+  init {
+    scope.launch(Dispatchers.Main) {
+      watchIsVisible()
+    }
+  }
+
+  private suspend fun watchIsVisible() {
+    var viewHolder: OverlayViewHolder? = null
+
+    isVisible.collect { isVisible ->
+      when (isVisible) {
+        true -> {
+          viewHolder = createOverlayViewHolder()
+          windowManager.addView(viewHolder!!.view, viewHolder!!.params)
+        }
+
+        false, null -> {
+          viewHolder?.view?.let {
+            try {
+              windowManager.removeView(it)
+              it.disposeComposition()
+            } catch (e: IllegalArgumentException) {
+              Log.e("OverlayViewController", "IllegalArgumentException $e")
+            }
+          }
+        }
+      }
+    }
+  }
+}
