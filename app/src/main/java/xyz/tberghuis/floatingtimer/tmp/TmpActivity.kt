986ab0e962983177d3ed6d97034379039456b45f
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-10-01 11:50:02 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpActivity.kt
@@ -0,0 +1,32 @@
+package xyz.tberghuis.floatingtimer.tmp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import xyz.tberghuis.floatingtimer.logd
+import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
+
+class TmpActivity : ComponentActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    logd("onCreate")
+
+    setContent {
+      FloatingTimerTheme {
+        Surface(
+          modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        ) {
+          TmpScreen()
+        }
+      }
+    }
+  }
+}
---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
@@ -0,0 +1,12 @@
+package xyz.tberghuis.floatingtimer.tmp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TmpScreen() {
+  Column {
+    Text("tmp screen")
+  }
+}
