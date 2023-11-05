4669b1213406c6ebbf5cdbd3cff2670535f1c717
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-11-06 08:58:49 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpVm.kt
@@ -6,9 +6,29 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.IBinder
 import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import xyz.tberghuis.floatingtimer.logd
+import xyz.tberghuis.floatingtimer.tmp2.FloatingService
 
 class TmpVm(private val application: Application) : AndroidViewModel(application) {
+
+  // init bind then .first()
+  private val floatingService = MutableStateFlow<FloatingService?>(null)
+
+  fun bindFloatingService() {
+    val floatingServiceIntent = Intent(application, FloatingService::class.java)
+    val connection = object : ServiceConnection {
+      override fun onServiceConnected(className: ComponentName, service: IBinder) {
+        val binder = service as FloatingService.LocalBinder
+        floatingService.value = binder.getService()
+      }
+      override fun onServiceDisconnected(arg0: ComponentName) {
+        floatingService.value = null
+      }
+    }
+    application.startForegroundService(floatingServiceIntent)
+    application.bindService(floatingServiceIntent, connection, 0)
+  }
 
 
 }
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/FloatingService.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/FloatingService.kt
@@ -1,5 +1,8 @@
 package xyz.tberghuis.floatingtimer.tmp2
 
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
 import androidx.lifecycle.LifecycleService
 import androidx.savedstate.SavedStateRegistry
 import androidx.savedstate.SavedStateRegistryController
@@ -8,10 +11,12 @@ import com.torrydo.screenez.ScreenEz
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import xyz.tberghuis.floatingtimer.logd
 
 
 class FloatingService : LifecycleService(), SavedStateRegistryOwner {
-    private val job = SupervisorJob()
+  private val job = SupervisorJob()
+
   // Main.immediate to prevent ANRs user input...
   val scope = CoroutineScope(Dispatchers.Default + job)
   lateinit var overlayController: OverlayController
@@ -20,6 +25,16 @@ class FloatingService : LifecycleService(), SavedStateRegistryOwner {
 
   override val savedStateRegistry: SavedStateRegistry
     get() = savedStateRegistryController.savedStateRegistry
+
+  private val binder = LocalBinder()
+  inner class LocalBinder : Binder() {
+    fun getService(): FloatingService = this@FloatingService
+  }
+  override fun onBind(intent: Intent): IBinder {
+    super.onBind(intent)
+    logd("onbind")
+    return binder
+  }
 
 
   override fun onCreate() {
