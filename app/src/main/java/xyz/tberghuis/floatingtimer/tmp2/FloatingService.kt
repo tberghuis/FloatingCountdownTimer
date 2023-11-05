7d385572e9b344d4e5a51b26130f8da8b1a9d28d
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-11-06 09:05:56 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/FloatingService.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/FloatingService.kt
@@ -1,8 +1,13 @@
 package xyz.tberghuis.floatingtimer.tmp2
 
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.savedstate.SavedStateRegistry
 import androidx.savedstate.SavedStateRegistryController
@@ -11,6 +16,15 @@ import com.torrydo.screenez.ScreenEz
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import xyz.tberghuis.floatingtimer.FOREGROUND_SERVICE_NOTIFICATION_ID
+import xyz.tberghuis.floatingtimer.INTENT_COMMAND
+import xyz.tberghuis.floatingtimer.INTENT_COMMAND_EXIT
+import xyz.tberghuis.floatingtimer.INTENT_COMMAND_RESET
+import xyz.tberghuis.floatingtimer.MainActivity
+import xyz.tberghuis.floatingtimer.NOTIFICATION_CHANNEL
+import xyz.tberghuis.floatingtimer.R
+import xyz.tberghuis.floatingtimer.REQUEST_CODE_EXIT
+import xyz.tberghuis.floatingtimer.REQUEST_CODE_RESET
 import xyz.tberghuis.floatingtimer.logd
 
 
@@ -27,9 +41,11 @@ class FloatingService : LifecycleService(), SavedStateRegistryOwner {
     get() = savedStateRegistryController.savedStateRegistry
 
   private val binder = LocalBinder()
+
   inner class LocalBinder : Binder() {
     fun getService(): FloatingService = this@FloatingService
   }
+
   override fun onBind(intent: Intent): IBinder {
     super.onBind(intent)
     logd("onbind")
@@ -45,6 +61,49 @@ class FloatingService : LifecycleService(), SavedStateRegistryOwner {
     savedStateRegistryController.performRestore(null)
 
     overlayController = OverlayController(this)
+  }
+
+  override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    super.onStartCommand(intent, flags, startId)
+    logd("FloatingService onStartCommand")
+    startForeground(FOREGROUND_SERVICE_NOTIFICATION_ID, buildNotification())
+    return START_NOT_STICKY
+  }
+
+  private fun buildNotification(): Notification {
+    val pendingIntent: PendingIntent =
+      Intent(this, MainActivity::class.java).let { notificationIntent ->
+        PendingIntent.getActivity(this, 0, notificationIntent, FLAG_IMMUTABLE)
+      }
+
+    val exitIntent = Intent(applicationContext, FloatingService::class.java)
+    exitIntent.putExtra(INTENT_COMMAND, INTENT_COMMAND_EXIT)
+    val exitPendingIntent = PendingIntent.getService(
+      applicationContext,
+      REQUEST_CODE_EXIT,
+      exitIntent,
+      FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE
+    )
+
+    val resetIntent = Intent(applicationContext, FloatingService::class.java)
+    resetIntent.putExtra(INTENT_COMMAND, INTENT_COMMAND_RESET)
+    val resetPendingIntent = PendingIntent.getService(
+      applicationContext,
+      REQUEST_CODE_RESET,
+      resetIntent,
+      FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE
+    )
+
+    val notification: Notification =
+      NotificationCompat.Builder(this, NOTIFICATION_CHANNEL).setContentTitle("Floating Timer")
+        .setSmallIcon(R.drawable.ic_alarm).setContentIntent(pendingIntent).addAction(
+          0, "Reset", resetPendingIntent
+        ).addAction(
+          0, "Exit", exitPendingIntent
+        )
+        .setForegroundServiceBehavior(NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE)
+        .build()
+    return notification
   }
 
 
