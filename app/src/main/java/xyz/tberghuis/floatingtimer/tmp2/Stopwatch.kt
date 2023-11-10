4dff4fa3c669dbfdee5a27319381d98d0f38c821
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-11-11 10:19:58 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/Stopwatch.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/Stopwatch.kt
@@ -2,9 +2,12 @@ package xyz.tberghuis.floatingtimer.tmp2
 
 import androidx.compose.runtime.mutableStateOf
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import xyz.tberghuis.floatingtimer.service.OverlayState
 import xyz.tberghuis.floatingtimer.service.stopwatch.StopwatchState
+import java.util.Timer
 import java.util.TimerTask
+import kotlin.concurrent.timerTask
 
 class Stopwatch(service: FloatingService) {
   //  val stopwatchState = StopwatchState(service.scope)
@@ -14,6 +17,28 @@ class Stopwatch(service: FloatingService) {
   val timeElapsed = mutableStateOf(0)
   val isRunningStateFlow = MutableStateFlow(false)
   var stopwatchIncrementTask: TimerTask? = null
+
+
+  // doitwrong
+  init {
+    service.scope.launch {
+      isRunningStateFlow.collect { running ->
+        when (running) {
+          true -> {
+            stopwatchIncrementTask = timerTask {
+              timeElapsed.value++
+            }
+            Timer().scheduleAtFixedRate(stopwatchIncrementTask, 1000, 1000)
+          }
+
+          false -> {
+            stopwatchIncrementTask?.cancel()
+            stopwatchIncrementTask = null
+          }
+        }
+      }
+    }
+  }
 
 
 }
