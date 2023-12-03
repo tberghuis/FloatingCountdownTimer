297cb6e804524037f97c0e436a46fb59aa0840db
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-04 10:32:19 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/errorsnackbar.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/errorsnackbar.kt
@@ -4,20 +4,37 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 import xyz.tberghuis.floatingtimer.logd
 
 @Composable
 fun ErrorSnackbarDemo() {
+
+  val snackbarHostState = remember { SnackbarHostState() }
+  val scope = rememberCoroutineScope()
+
   Scaffold(
     modifier = Modifier,
-    snackbarHost = {},
+    snackbarHost = {
+      SnackbarHost(snackbarHostState)
+    },
   ) { padding ->
     Column(modifier = Modifier.padding(padding)) {
       Button(onClick = {
         logd("ErrorSnackbarDemo")
+        scope.launch {
+          snackbarHostState.showSnackbar(
+            "error message here"
+          )
+        }
+
       }) {
         Text("error")
       }
