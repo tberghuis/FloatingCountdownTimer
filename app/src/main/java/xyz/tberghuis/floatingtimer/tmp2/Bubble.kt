fb3b587cdc47e37deb767eec79325f7f98c51f97
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-05 10:19:58 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/Bubble.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/Bubble.kt
@@ -3,34 +3,14 @@ package xyz.tberghuis.floatingtimer.tmp2
 import android.util.Log
 
 abstract class Bubble(private val service: FloatingService) {
-  // todo move ViewHolder logic to here
   val viewHolder = TimerViewHolder(service)
-
   open fun exit() {
-
-//    service.overlayController.bubbleSet.iterator(). remove(this)
-
-
-    // todo move logic to overlaycontroller
-    //  bubbleSet empty stop service
     try {
       service.overlayController.windowManager.removeView(viewHolder.view)
     } catch (e: IllegalArgumentException) {
-      Log.e("Stopwatch", "IllegalArgumentException $e")
+      Log.e("Bubble", "IllegalArgumentException $e")
     }
   }
-
   abstract fun reset()
-
   abstract fun onTap()
-
-// todo or MVI
-//  fun exitBubble() {
-//    try {
-//      service.overlayController.windowManager.removeView(viewHolder.view)
-//    } catch (e: IllegalArgumentException) {
-//      Log.e("Stopwatch", "IllegalArgumentException $e")
-//    }
-//  }
-
 }
