22b6a3588e7d468edef2af734cc08a6b39230ba0
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-05 15:26:28 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/screens/SettingsScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/screens/SettingsScreen.kt
@@ -1,10 +1,8 @@
 package xyz.tberghuis.floatingtimer.screens
 
-import android.app.Activity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,18 +13,15 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,7 +37,6 @@ import xyz.tberghuis.floatingtimer.LocalNavController
 import xyz.tberghuis.floatingtimer.PROGRESS_ARC_WIDTH
 import xyz.tberghuis.floatingtimer.R
 import xyz.tberghuis.floatingtimer.TIMER_SIZE_DP
-import xyz.tberghuis.floatingtimer.logd
 import xyz.tberghuis.floatingtimer.service.countdown.ProgressArc
 import xyz.tberghuis.floatingtimer.tmp2.PremiumDialog
 import xyz.tberghuis.floatingtimer.tmp2.SettingsViewModel
@@ -53,7 +47,6 @@ fun SettingsScreen(
   vm: SettingsViewModel = viewModel()
 ) {
   val navController = LocalNavController.current
-  val context = LocalContext.current
 
   Scaffold(
     modifier = Modifier,
