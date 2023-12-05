ca4b5f7bcb25ae331f33762a3dd9748f5682e152
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-05 16:17:14 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/service/OverlayController.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/service/OverlayController.kt
@@ -20,7 +20,6 @@ import xyz.tberghuis.floatingtimer.service.countdown.Countdown
 import xyz.tberghuis.floatingtimer.service.countdown.CountdownView
 import xyz.tberghuis.floatingtimer.service.stopwatch.Stopwatch
 import xyz.tberghuis.floatingtimer.service.stopwatch.StopwatchView
-import xyz.tberghuis.floatingtimer.tmp2.TrashController
 import kotlin.math.max
 import kotlin.math.min
 
---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/service/TrashController.kt
@@ -0,0 +1,78 @@
+package xyz.tberghuis.floatingtimer.service
+
+import android.graphics.PixelFormat
+import android.util.Log
+import android.view.WindowManager
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.IntOffset
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import xyz.tberghuis.floatingtimer.composables.LocalFloatingService
+import xyz.tberghuis.floatingtimer.tmp2.TrashOverlay
+
+// todo move windowManager to service
+class TrashController(
+  val windowManager: WindowManager,
+  private val service: FloatingService,
+) {
+  val isBubbleDragging = MutableStateFlow(false)
+
+  //  val bubbleDraggingPosition = MutableStateFlow(IntOffset.Zero)
+  val bubbleDraggingPosition = mutableStateOf(IntOffset.Zero)
+
+  // todo make generic
+  var isBubbleHoveringTrash = false
+
+  private var overlay: ComposeView? = null
+  private val params: WindowManager.LayoutParams = WindowManager.LayoutParams(
+    WindowManager.LayoutParams.MATCH_PARENT,
+    WindowManager.LayoutParams.MATCH_PARENT,
+    WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+    PixelFormat.TRANSLUCENT
+  )
+
+  init {
+    service.scope.launch(Dispatchers.Main) {
+      addRemoveTrashOverlay()
+    }
+  }
+
+  private suspend fun addRemoveTrashOverlay() {
+    isBubbleDragging.collect { isVisible ->
+      when (isVisible) {
+        true -> {
+          val newOverlay = createTrashView()
+          overlay = newOverlay
+          windowManager.addView(newOverlay, params)
+        }
+
+        false -> {
+          overlay?.let {
+            try {
+              windowManager.removeView(it)
+              // do i need disposecomposition???
+            } catch (e: IllegalArgumentException) {
+              Log.e("OverlayViewController", "IllegalArgumentException $e")
+            } finally {
+              overlay = null
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private fun createTrashView(): ComposeView {
+    val view = createComposeView(service)
+    view.setContent {
+      CompositionLocalProvider(LocalFloatingService provides service) {
+        TrashOverlay()
+      }
+    }
+    return view
+  }
+}
