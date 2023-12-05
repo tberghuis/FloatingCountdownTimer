94f526ce401f9feac42692ec70dad2d9b1b01cdb
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-05 15:32:56 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
@@ -1,13 +1,9 @@
 package xyz.tberghuis.floatingtimer.tmp
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
-import xyz.tberghuis.floatingtimer.R
-import xyz.tberghuis.floatingtimer.tmp2.PremiumDialog
 
 @Composable
 fun TmpScreen(
@@ -15,30 +11,5 @@ fun TmpScreen(
 ) {
   Column {
     Text("tmp screen")
-
-    Button(onClick = {
-      vm.addStopwatch()
-    }) {
-      Text("addStopwatch")
-    }
-
-    Button(onClick = {
-      vm.addCountdown()
-    }) {
-      Text("addCountdown")
-    }
-
-    Button(onClick = {
-      vm.exitAll()
-    }) {
-      Text("exit all timers")
-    }
-
-    Button(onClick = {
-      vm.premiumVmc.showPurchaseDialog = true
-    }) {
-      Text("show dialog")
-    }
   }
-  PremiumDialog(vm.premiumVmc, stringResource(R.string.premium_reason_multiple_timers))
 }
