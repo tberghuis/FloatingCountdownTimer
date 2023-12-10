567f56ef9f23df81a7a2a2abac2409ff58529c3d
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-11 08:44:02 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
@@ -1,22 +1,33 @@
 package xyz.tberghuis.floatingtimer.tmp
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun TmpScreen(
   vm: TmpVm = viewModel()
 ) {
-  Column {
+  Column(
+    modifier = Modifier.padding(horizontal = 30.dp),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally,
+  ) {
     Text("resize timer demo")
+    TmpTimer()
     TmpSlider()
   }
 }
-
 
 @Composable
 fun TmpSlider(
@@ -30,4 +41,13 @@ fun TmpSlider(
     modifier = Modifier,
     valueRange = 1f..2f,
   )
+}
+
+@Composable
+fun TmpTimer() {
+  Box(
+    modifier = Modifier.border(1.dp, Color.Black),
+  ) {
+    Text("00:59")
+  }
 }
