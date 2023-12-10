c295c5142db8c5a13e2bf7b38039b460368508e2
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-11 08:28:36 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
@@ -1,8 +1,10 @@
 package xyz.tberghuis.floatingtimer.tmp
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -11,5 +13,21 @@ fun TmpScreen(
 ) {
   Column {
     Text("resize timer demo")
+    TmpSlider()
   }
+}
+
+
+@Composable
+fun TmpSlider(
+  vm: TmpVm = viewModel()
+) {
+  Slider(
+    value = vm.timerSizeFactor,
+    onValueChange = {
+      vm.timerSizeFactor = it
+    },
+    modifier = Modifier,
+    valueRange = 1f..2f,
+  )
 }
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpVm.kt
@@ -1,11 +1,13 @@
 package xyz.tberghuis.floatingtimer.tmp
 
 import android.app.Application
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 
 class TmpVm(private val application: Application) : AndroidViewModel(application) {
-  val timerSizeFactor = mutableFloatStateOf(1f)
+  var timerSizeFactor by mutableFloatStateOf(1f)
 
 }
