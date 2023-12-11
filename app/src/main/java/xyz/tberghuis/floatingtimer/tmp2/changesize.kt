04e8fea89344e32b77f75e2f42fda635d7a47f92
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-12 07:26:04 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/changesize.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/changesize.kt
@@ -1,12 +1,53 @@
 package xyz.tberghuis.floatingtimer.tmp2
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import xyz.tberghuis.floatingtimer.LocalNavController
+import xyz.tberghuis.floatingtimer.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChangeSizeScreen() {
-  Column {
-    Text("ChangeSizeScreen")
+  val navController = LocalNavController.current
+
+  Scaffold(
+    modifier = Modifier,
+    topBar = {
+      TopAppBar(
+        title = { Text(stringResource(R.string.change_size)) },
+        navigationIcon = {
+          IconButton(onClick = {
+            navController.navigateUp()
+          }) {
+            Icon(Icons.Filled.ArrowBack, stringResource(R.string.back))
+          }
+        },
+        modifier = Modifier,
+      )
+    },
+    snackbarHost = {},
+  ) { padding ->
+    Column(
+      modifier = Modifier
+        .padding(padding)
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState()),
+    ) {
+      Text("ChangeSizeScreen")
+    }
   }
 }
