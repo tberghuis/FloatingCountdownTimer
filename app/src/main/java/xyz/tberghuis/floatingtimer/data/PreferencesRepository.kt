8883adefb6aa9da0bc06f0573719630bf108d934
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-13 15:49:15 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/data/PreferencesRepository.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/data/PreferencesRepository.kt
@@ -10,7 +10,6 @@ import androidx.datastore.preferences.core.floatPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import xyz.tberghuis.floatingtimer.logd
 
