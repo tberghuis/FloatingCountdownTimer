b30be1540e5be079faf511d23d9ffb147168139b
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-13 14:03:09 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/screens/ColorSettingScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/screens/ColorSettingScreen.kt
@@ -33,7 +33,7 @@ import xyz.tberghuis.floatingtimer.R
 import xyz.tberghuis.floatingtimer.composables.LocalHaloColour
 import xyz.tberghuis.floatingtimer.composables.PremiumDialog
 import xyz.tberghuis.floatingtimer.tmp2.SettingsTimerPreviewCard
-import xyz.tberghuis.floatingtimer.tmp3.ColorSettingViewModel
+import xyz.tberghuis.floatingtimer.viewmodels.ColorSettingViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
