b1e8f8370c749524dd28449230b6acb5233f46e1
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-13 15:50:46 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/screens/HomeScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/screens/HomeScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
