9f74fed6a51c25da78eac457e39edc8b5755240b
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-13 14:34:12 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/service/countdown/CountdownView.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/service/countdown/CountdownView.kt
@@ -9,7 +9,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.zIndex
 import xyz.tberghuis.floatingtimer.common.TimeDisplay
 import xyz.tberghuis.floatingtimer.service.BubbleProperties
-import xyz.tberghuis.floatingtimer.tmp2.TmpProgressArc
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.Dp
+import xyz.tberghuis.floatingtimer.composables.LocalHaloColour
 
 //@Composable
 //fun CountdownView(countdown: Countdown) {
@@ -46,7 +52,49 @@ fun CountdownViewDisplay(
       .zIndex(1f),
     contentAlignment = Alignment.Center
   ) {
-    TmpProgressArc(timeLeftFraction, bubbleProperties.arcWidth)
+    CountdownProgressArc(timeLeftFraction, bubbleProperties.arcWidth)
     TimeDisplay(countdownSeconds, bubbleProperties.fontSize)
+  }
+}
+
+@Composable
+fun CountdownProgressArc(timeLeftFraction: Float, arcWidth: Dp) {
+  val sweepAngle = 360 * timeLeftFraction
+  val haloColour = LocalHaloColour.current
+
+  Canvas(
+    Modifier.fillMaxSize()
+  ) {
+    // background
+    // todo make partial transparent
+    drawCircle(
+      color = Color.White,
+    )
+    drawArc(
+      color = Color.White,
+      startAngle = 0f,
+      sweepAngle = 360f,
+      useCenter = false,
+      style = Stroke(arcWidth.toPx()),
+      size = Size(size.width, size.height)
+    )
+
+    drawArc(
+      color = haloColour.copy(alpha = .1f),
+      startAngle = 0f,
+      sweepAngle = 360f,
+      useCenter = false,
+      style = Stroke(arcWidth.toPx()),
+      size = Size(size.width, size.height)
+    )
+
+    drawArc(
+      color = haloColour,
+      -90f,
+      sweepAngle,
+      false,
+      style = Stroke(arcWidth.toPx()),
+      size = Size(size.width, size.height)
+    )
   }
 }
