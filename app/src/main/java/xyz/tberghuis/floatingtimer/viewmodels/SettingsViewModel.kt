966f0eb160ee7acc67ceea6ff4a8609bd872db05
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-13 13:53:58 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/viewmodels/SettingsViewModel.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/viewmodels/SettingsViewModel.kt
@@ -11,34 +11,3 @@ import kotlinx.coroutines.launch
 import xyz.tberghuis.floatingtimer.logd
 import xyz.tberghuis.floatingtimer.providePreferencesRepository
 
-class SettingsViewModel(application: Application) : AndroidViewModel(application) {
-  private val preferences = application.providePreferencesRepository()
-  var colorPickerColorState = mutableStateOf(HsvColor.from(Color.Blue))
-
-  val premiumVmc = PremiumVmc(application, viewModelScope)
-
-  private val premiumFlow = application.providePreferencesRepository().haloColourPurchasedFlow
-
-  init {
-    viewModelScope.launch {
-      colorPickerColorState.value = HsvColor.from(preferences.haloColourFlow.first())
-    }
-  }
-
-  fun saveHaloColor() {
-    viewModelScope.launch {
-      preferences.updateHaloColour(colorPickerColorState.value.toColor())
-    }
-  }
-
-  fun saveHaloColorClick() {
-    viewModelScope.launch {
-      logd("saveHaloColorClick")
-      if (premiumFlow.first()) {
-        saveHaloColor()
-      } else {
-        premiumVmc.showPurchaseDialog = true
-      }
-    }
-  }
-}
