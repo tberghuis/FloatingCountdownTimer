4238479853659d9d2ec637216de406be8d9fb25a
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-08 07:31:09 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpChangeColor.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpChangeColor.kt
@@ -1,10 +1,12 @@
 package xyz.tberghuis.floatingtimer.tmp3
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import xyz.tberghuis.floatingtimer.logd
 
 @Composable
 fun TmpChangeColor(
@@ -12,9 +14,20 @@ fun TmpChangeColor(
 ) {
   Column {
     Text("tmp change color")
+    Button(onClick = {
+      vm.setResultAndPopHome()
+    }) {
+      Text("button")
+    }
   }
 }
 
 class TmpChangeColorVm : ViewModel() {
   val fdfsd = "fdsfs"
+
+
+  fun setResultAndPopHome() {
+    logd("setResultAndPopHome")
+  }
+
 }
