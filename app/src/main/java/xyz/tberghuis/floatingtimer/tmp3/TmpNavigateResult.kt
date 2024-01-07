df498d57ec1b9a39e30f32eee0e1e9af26eac83c
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-08 07:14:22 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpNavigateResult.kt
@@ -0,0 +1,12 @@
+package xyz.tberghuis.floatingtimer.tmp3
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TmpNavigateResult() {
+  Column {
+    Text("tmp nav result")
+  }
+}
