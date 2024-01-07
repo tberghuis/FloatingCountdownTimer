7071b9188ab517c3d495c5694f06abcd6a4fd006
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-08 07:28:26 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpNavHost.kt
@@ -0,0 +1,27 @@
+package xyz.tberghuis.floatingtimer.tmp3
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import xyz.tberghuis.floatingtimer.LocalNavController
+
+@Composable
+fun TmpNavHost(
+) {
+  val navController = rememberNavController()
+  CompositionLocalProvider(LocalNavController provides navController) {
+    NavHost(
+      navController = navController, startDestination = "home"
+    ) {
+      composable("home") {
+        TmpNavigateResult()
+      }
+
+      composable("change_color") {
+        TmpChangeColor()
+      }
+    }
+  }
+}
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpNavigateResult.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpNavigateResult.kt
@@ -4,34 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import xyz.tberghuis.floatingtimer.LocalNavController
-
-
-@Composable
-fun TmpNavHost(
-) {
-  val navController = rememberNavController()
-  CompositionLocalProvider(LocalNavController provides navController) {
-    NavHost(
-      navController = navController, startDestination = "home"
-    ) {
-      composable("home") {
-        TmpNavigateResult()
-      }
-
-      composable("change_color") {
-        TmpChangeColor()
-      }
-    }
-  }
-}
-
 
 @Composable
 fun TmpNavigateResult(
