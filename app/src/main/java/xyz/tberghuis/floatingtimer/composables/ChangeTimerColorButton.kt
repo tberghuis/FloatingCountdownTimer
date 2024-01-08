ee94245c67a55ed5b26b48c3396687b4c1b56cb3
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-09 10:37:38 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/composables/ChangeTimerColorButton.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/composables/ChangeTimerColorButton.kt
@@ -1,14 +1,17 @@
 package xyz.tberghuis.floatingtimer.composables
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import xyz.tberghuis.floatingtimer.LocalNavController
@@ -17,16 +20,26 @@ import xyz.tberghuis.floatingtimer.R
 @Composable
 fun ChangeTimerColorButton(route: String, haloColor: Color) {
   val nav = LocalNavController.current
-  Row(
-    Modifier.clickable {
-      nav.navigate(route)
-    }
+  Box(
+    Modifier
+      .clickable {
+        nav.navigate(route)
+      }
+      .size(50.dp),
+    contentAlignment = Alignment.Center,
   ) {
-    Box(
-      Modifier
-        .size(50.dp)
-        .background(haloColor)
-    )
+    Canvas(
+      Modifier.fillMaxSize()
+    ) {
+      drawArc(
+        color = haloColor,
+        startAngle = 0f,
+        sweepAngle = 360f,
+        useCenter = false,
+        style = Stroke(10.dp.toPx()),
+        size = Size(size.width, size.height)
+      )
+    }
     Image(
       painterResource(R.drawable.palette_icon),
       contentDescription = "",
