c1111662f1b3c3354db87d4f5ed2577585381255
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-09 12:16:46 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/screens/ColorSettingScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/screens/ColorSettingScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.godaddy.android.colorpicker.ClassicColorPicker
