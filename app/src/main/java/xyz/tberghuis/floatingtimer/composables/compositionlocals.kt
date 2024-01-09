7696b2848c787b7067c01c95fa195491baf8a7c9
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-09 12:10:39 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/composables/compositionlocals.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/composables/compositionlocals.kt
@@ -2,12 +2,7 @@ package xyz.tberghuis.floatingtimer.composables
 
 import androidx.compose.runtime.compositionLocalOf
 import xyz.tberghuis.floatingtimer.service.FloatingService
-import androidx.compose.ui.graphics.Color
 
 val LocalFloatingService = compositionLocalOf<FloatingService> {
   error("CompositionLocal LocalFloatingService not present")
 }
-
-//val LocalHaloColour = compositionLocalOf<Color> {
-//  error("CompositionLocal LocalHaloColour not present")
-//}
