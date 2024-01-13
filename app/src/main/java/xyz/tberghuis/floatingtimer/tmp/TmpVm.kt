0c8da435fa02fcfbda2bc83329f2e92e4d87f1ef
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-14 07:30:20 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpVm.kt
@@ -3,10 +3,10 @@ package xyz.tberghuis.floatingtimer.tmp
 import android.app.Application
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 
+// why no compiler error "application" ??? was happening in NoteBoat
 class TmpVm(private val application: Application) : AndroidViewModel(application) {
 
   var progress by mutableFloatStateOf(0f)
