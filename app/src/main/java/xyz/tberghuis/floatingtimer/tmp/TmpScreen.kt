c723926e9abe4e4a88dbb3e68cd2a501022cc528
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-14 10:57:11 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
@@ -3,17 +3,15 @@ package xyz.tberghuis.floatingtimer.tmp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun TmpScreen(
-  vm: TmpVm = viewModel()
+//  vm: TmpVm = viewModel()
 ) {
   Column(
     modifier = Modifier.padding(horizontal = 30.dp),
@@ -21,19 +19,5 @@ fun TmpScreen(
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
     Text("tmp screen")
-
-    Button(onClick = {
-      vm.initialiseRingtone()
-    }) {
-      Text("init ringtone")
-    }
-
-    Button(onClick = {
-      vm.playRingtone()
-    }) {
-      Text("play ringtone")
-    }
-
-
   }
 }
