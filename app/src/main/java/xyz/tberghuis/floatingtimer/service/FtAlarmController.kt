f72f0fd8ae4b763d1d97d0332ef83bedb300ce0f
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-19 15:57:10 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/service/FtAlarmController.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/service/FtAlarmController.kt
@@ -1,11 +1,14 @@
 package xyz.tberghuis.floatingtimer.service
 
+import android.media.AudioAttributes
+import android.media.AudioManager
 import android.media.Ringtone
 import android.media.RingtoneManager
 import android.os.Build
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import xyz.tberghuis.floatingtimer.service.countdown.Countdown
 
 class FtAlarmController(
@@ -16,36 +19,49 @@ class FtAlarmController(
 
   init {
     val notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-    ringtone = RingtoneManager.getRingtone(floatingService, notification)
+    ringtone = RingtoneManager.getRingtone(floatingService, notification).apply {
+      audioAttributes = AudioAttributes.Builder()
+        .setUsage(AudioAttributes.USAGE_ALARM)
+        .setLegacyStreamType(AudioManager.STREAM_ALARM)
+        .build()
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        isHapticGeneratorEnabled = false
+      }
+    }
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
       ringtone?.isLooping = true
     }
-    floatingService.scope.launch(Main.immediate) {
+    floatingService.scope.launch {
       try {
         finishedCountdowns.collect {
-          when (it.size) {
-            0 -> {
-              if (ringtone?.isPlaying == true) {
-                ringtone?.stop()
+          withContext(Main.immediate) {
+            when (it.size) {
+              0 -> {
+                if (ringtone?.isPlaying == true) {
+                  ringtone?.stop()
+                }
               }
-            }
-            // allow for single play ringtone on older apilevels < 28
-            else -> {
-              if (ringtone?.isPlaying == false) {
-                ringtone?.play()
+              // allow for single play ringtone on older apilevels < 28
+              else -> {
+                if (ringtone?.isPlaying == false) {
+                  ringtone?.play()
+                }
               }
             }
           }
         }
       } finally {
         // scope is cancelled
-        ringtone?.stop()
+        withContext(Main.immediate) {
+          if (ringtone?.isPlaying == true) {
+            ringtone?.stop()
+          }
+        }
       }
     }
   }
 
   fun startAlarm(c: Countdown) {
-//    finishedCountdowns.value += setOf(c)
     finishedCountdowns.value += c
   }
 
