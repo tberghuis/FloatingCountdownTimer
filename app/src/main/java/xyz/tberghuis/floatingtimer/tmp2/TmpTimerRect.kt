a8bb74b9821cd14be402ca225f217e21aacf0fb2
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-20 11:38:10 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/TmpTimerRect.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/TmpTimerRect.kt
@@ -1,27 +1,40 @@
 package xyz.tberghuis.floatingtimer.tmp2
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun TmpTimerRect() {
-
-  Column(
+  // represents window.addView size....
+  Box(
     modifier = Modifier
-      .background(Color.Cyan)
-      .size(100.dp),
-    verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally,
+      .padding(5.dp),
+    contentAlignment = Alignment.Center,
   ) {
-    Text("00:59")
-  }
 
+    Surface(
+      modifier = Modifier,
+      shape = RoundedCornerShape(10.dp),
+      shadowElevation = 5.dp,
+    ) {
+      Column(
+        modifier = Modifier
+          .size(100.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+      ) {
+        Text("00:59")
+      }
+    }
+  }
 }
