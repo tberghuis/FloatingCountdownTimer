1ff16a0ca97f7710fb553cf820cb32051c855d70
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-30 14:34:49 +1100

    .

--- app/src/androidTest/java/xyz/tberghuis/floatingtimer/ScreenshotTest.kt
+++ app/src/androidTest/java/xyz/tberghuis/floatingtimer/ScreenshotTest.kt
@@ -1,39 +1,23 @@
 package xyz.tberghuis.floatingtimer
 
+import android.graphics.Bitmap
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.core.app.takeScreenshot
 import androidx.test.core.graphics.writeToTestStorage
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
-import androidx.test.espresso.screenshot.captureToBitmap
-import androidx.test.ext.junit.rules.activityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestName
-import org.junit.runner.RunWith
+import java.io.FileOutputStream
 import java.io.IOException
 
-
-@RunWith(AndroidJUnit4::class)
 class ScreenshotTest {
 
   @get:Rule
   var nameRule = TestName()
 
   @get:Rule
-  val activityScenarioRule = activityScenarioRule<MainActivity>()
-
-
-//  @Test
-//  @Throws(IOException::class)
-//  fun saveActivityBitmap() {
-////    onView(isRoot())
-////      .captureToBitmap()
-////      .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}")
-//  }
-
+  val composeTestRule = createAndroidComposeRule<MainActivity>()
 
   @Test
   @Throws(IOException::class)
@@ -41,4 +25,28 @@ class ScreenshotTest {
     takeScreenshot()
       .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}")
   }
+
+
+  @Test
+  @Throws(IOException::class)
+  fun willitblend() {
+    composeTestRule.apply {
+//      val bitmap = onRoot().captureToImage().asAndroidBitmap()
+//      saveScreenshot("willitblend", bitmap)
+
+      takeScreenshot()
+        .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}")
+    }
+  }
+
+
+}
+
+
+private fun saveScreenshot(filename: String, bmp: Bitmap) {
+  val path = InstrumentationRegistry.getInstrumentation().targetContext.filesDir.canonicalPath
+  FileOutputStream("$path/$filename.png").use { out ->
+    bmp.compress(Bitmap.CompressFormat.PNG, 100, out)
+  }
+  println("Saved screenshot to $path/$filename.png")
 }
