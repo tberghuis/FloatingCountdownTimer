32e0c41f456e556570de8e4b1bc400accef309ac
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-03 14:05:16 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
@@ -3,13 +3,11 @@ package xyz.tberghuis.floatingtimer.tmp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import xyz.tberghuis.floatingtimer.tmp2.TmpTimerRect
-import xyz.tberghuis.floatingtimer.tmp3.TmpTimerPause
 
 @Composable
 fun TmpScreen(
@@ -22,6 +20,5 @@ fun TmpScreen(
   ) {
 //    Text("tmp screen")
     TmpTimerRect()
-    TmpTimerPause()
   }
 }
