cddbde0bd7b8e72058095d3ef4809cdb6b587eb0
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-05 08:50:45 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpHomeScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpHomeScreen.kt
@@ -1,9 +1,28 @@
 package xyz.tberghuis.floatingtimer.tmp3
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun TmpHomeScreen() {
-  Text("hello home")
+  // doitwrong, repeat scaffold for each route
+  Scaffold(
+    modifier = Modifier,
+    topBar = {},
+    bottomBar = {},
+  ) { padding ->
+    Column(
+      modifier = Modifier.padding(padding),
+      verticalArrangement = Arrangement.Top,
+      horizontalAlignment = Alignment.Start,
+    ) {
+      Text("hello home")
+    }
+  }
 }
