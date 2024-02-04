9cfaa8e62246ca0a93b4114d0a9526cff826d245
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-05 08:36:30 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpNavHost.kt
@@ -0,0 +1,26 @@
+package xyz.tberghuis.floatingtimer.tmp3
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import xyz.tberghuis.floatingtimer.LocalNavController
+
+@Composable
+fun TmpNavHost() {
+  val navController = rememberNavController()
+  CompositionLocalProvider(LocalNavController provides navController) {
+    NavHost(
+      navController = navController, startDestination = "home"
+    ) {
+      composable("home") {
+        TmpHomeScreen()
+      }
+      composable("settings") {
+        TmpSettingsScreen()
+      }
+    }
+  }
+
+}
---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpSettingsScreen.kt
@@ -0,0 +1,9 @@
+package xyz.tberghuis.floatingtimer.tmp3
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TmpSettingsScreen() {
+  Text("hello settings")
+}
