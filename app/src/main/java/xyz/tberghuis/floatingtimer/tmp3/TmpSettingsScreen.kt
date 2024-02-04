3ef2a8f99941f44d2b96e111d126b57e774ffb4c
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-05 10:26:54 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpSettingsScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpSettingsScreen.kt
@@ -2,8 +2,45 @@ package xyz.tberghuis.floatingtimer.tmp3
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import xyz.tberghuis.floatingtimer.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TmpSettingsScreen() {
-  Text("hello settings")
+  Scaffold(
+    modifier = Modifier,
+    topBar = {
+      TopAppBar(
+        title = { Text("Settings") },
+        navigationIcon = {
+          IconButton(onClick = {
+          }) {
+            Icon(Icons.AutoMirrored.Filled.ArrowBack, stringResource(R.string.back))
+          }
+        },
+        modifier = Modifier,
+      )
+    },
+  ) { padding ->
+    Column(
+      modifier = Modifier.padding(padding),
+      verticalArrangement = Arrangement.Top,
+      horizontalAlignment = Alignment.Start,
+    ) {
+      Text("hello settings")
+    }
+  }
 }
