91f69ee1287b9be5738d7561a055b83bc7a105c2
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-12 15:45:49 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpCountdownScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpCountdownScreen.kt
@@ -1,11 +1,6 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
-import android.provider.Settings
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,12 +8,8 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -28,16 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.lifecycle.viewmodel.compose.viewModel
-import xyz.tberghuis.floatingtimer.R
-import xyz.tberghuis.floatingtimer.REQUEST_CODE_ACTION_MANAGE_OVERLAY_PERMISSION
 import xyz.tberghuis.floatingtimer.logd
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -45,8 +27,6 @@ import xyz.tberghuis.floatingtimer.logd
 fun TmpCountdownScreen(
   vm: TmpCountdownScreenVm = viewModel()
 ) {
-  val context = LocalContext.current
-
   Scaffold(
     modifier = Modifier,
     topBar = {
@@ -61,39 +41,7 @@ fun TmpCountdownScreen(
   ) { padding ->
     TmpCountdownScreenContent(padding)
   }
-
-  if (vm.grantOverlayVmc.showGrantOverlayDialog) {
-    AlertDialog(onDismissRequest = {
-      vm.grantOverlayVmc.showGrantOverlayDialog = false
-    }, confirmButton = {
-      Button(onClick = {
-        logd("go to settings")
-        val intent = Intent(
-          Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + context.packageName)
-        )
-
-        startActivityForResult(
-          context as Activity, intent, REQUEST_CODE_ACTION_MANAGE_OVERLAY_PERMISSION, null
-        )
-        vm.grantOverlayVmc.showGrantOverlayDialog = false
-
-      }) {
-        Text(stringResource(R.string.go_to_settings))
-      }
-    }, title = {
-      Text(stringResource(R.string.enable_overlay_permission))
-    }, text = {
-      Text(buildAnnotatedString {
-        append(stringResource(R.string.dialog_enable_overlay_permission))
-        append(" ")
-        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-          append(stringResource(R.string.app_name))
-        }
-      })
-    })
-  }
-
-
+  TmpGrantOverlayDialog(vm.grantOverlayVmc)
 }
 
 @Composable
---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpGrantOverlayDialog.kt
@@ -0,0 +1,57 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import androidx.compose.runtime.Composable
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.core.app.ActivityCompat.startActivityForResult
+import xyz.tberghuis.floatingtimer.R
+import xyz.tberghuis.floatingtimer.REQUEST_CODE_ACTION_MANAGE_OVERLAY_PERMISSION
+import xyz.tberghuis.floatingtimer.logd
+
+@Composable
+fun TmpGrantOverlayDialog(
+  grantOverlayVmc: TmpGrantOverlayVmc
+) {
+  val context = LocalContext.current
+  if (grantOverlayVmc.showGrantOverlayDialog) {
+    AlertDialog(onDismissRequest = {
+      grantOverlayVmc.showGrantOverlayDialog = false
+    }, confirmButton = {
+      Button(onClick = {
+        logd("go to settings")
+        val intent = Intent(
+          Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + context.packageName)
+        )
+
+        startActivityForResult(
+          context as Activity, intent, REQUEST_CODE_ACTION_MANAGE_OVERLAY_PERMISSION, null
+        )
+        grantOverlayVmc.showGrantOverlayDialog = false
+
+      }) {
+        Text(stringResource(R.string.go_to_settings))
+      }
+    }, title = {
+      Text(stringResource(R.string.enable_overlay_permission))
+    }, text = {
+      Text(buildAnnotatedString {
+        append(stringResource(R.string.dialog_enable_overlay_permission))
+        append(" ")
+        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+          append(stringResource(R.string.app_name))
+        }
+      })
+    })
+  }
+}
