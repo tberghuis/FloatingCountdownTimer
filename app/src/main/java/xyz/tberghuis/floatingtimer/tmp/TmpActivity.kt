2f823e4aebfd8e8e752e7bc4922a63249b72f35d
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-14 12:31:54 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpActivity.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import xyz.tberghuis.floatingtimer.logd
-import xyz.tberghuis.floatingtimer.tmp3.TmpSettingsNavHost
 import xyz.tberghuis.floatingtimer.tmp4.TmpNavHost
 import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
 
