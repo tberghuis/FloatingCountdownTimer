5a54b40b7fc1d46efe3b3d1a4724484961a10c10
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-16 10:48:49 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpStopwatchScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpStopwatchScreen.kt
@@ -1,4 +1,46 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
-class TmpStopwatchScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import xyz.tberghuis.floatingtimer.R
+import xyz.tberghuis.floatingtimer.composables.PremiumDialog
+
+@Composable
+fun TmpStopwatchScreen(
+  vm: TmpStopwatchScreenVm = viewModel()
+) {
+  val focusManager = LocalFocusManager.current
+
+  Scaffold(
+    modifier = Modifier,
+    topBar = { TmpFtTopAppBar() },
+    bottomBar = {
+      TmpBottomBar(TmpScreenTypeStopwatch)
+    },
+  ) { padding ->
+    TmpStopwatchScreenContent(padding)
+  }
+  PremiumDialog(vm.premiumVmc, stringResource(R.string.premium_reason_multiple_timers))
+}
+
+@Composable
+fun TmpStopwatchScreenContent(padding: PaddingValues) {
+  Column(
+    modifier = Modifier
+      .padding(padding)
+      .verticalScroll(rememberScrollState())
+  ) {
+    TmpCreateStopwatchCard()
+//    TmpSavedCountdownCard()
+  }
+//  ConfirmDeleteSavedTimerDialog()
 }
