6ade64cc29b950053bf63240786fd59c2c423c54
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-16 09:36:19 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpGrantOverlayVmc.kt
+++
@@ -1,9 +0,0 @@
-package xyz.tberghuis.floatingtimer.tmp4
-
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-
-class TmpGrantOverlayVmc {
-    var showGrantOverlayDialog by mutableStateOf(false)
-}
