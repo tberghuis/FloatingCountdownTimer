11a8a757cb99997f0a4370f9aad5f8a904248728
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-16 09:51:09 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpCreateStopwatchCard.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpCreateStopwatchCard.kt
@@ -1,6 +1,7 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
 import android.provider.Settings
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,45 +19,50 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import xyz.tberghuis.floatingtimer.R
+import xyz.tberghuis.floatingtimer.composables.ChangeTimerColorButton
 import xyz.tberghuis.floatingtimer.logd
 
-//@Composable
-//fun TmpCreateStopwatchCard() {
-//  val context = LocalContext.current
-//
-////  val vm: HomeViewModel = viewModel()
-//  ElevatedCard(
-//    modifier = Modifier
-//      .fillMaxWidth()
-//      .padding(15.dp),
-//  ) {
-//    Row(
-//      modifier = Modifier
-//        .padding(10.dp)
-//        .fillMaxWidth(), horizontalArrangement = Arrangement.Center
-//    ) {
-//      Text(stringResource(id = R.string.stopwatch), fontSize = 20.sp)
-//    }
-//    Row(
-//      modifier = Modifier
-//        .padding(10.dp)
-//        .fillMaxWidth(),
-//      horizontalArrangement = Arrangement.Center,
-//      verticalAlignment = Alignment.CenterVertically,
-//    ) {
+@Composable
+fun TmpCreateStopwatchCard() {
+  val context = LocalContext.current
+
+  val sharedVm: TmpSharedVm = viewModel(LocalContext.current as ComponentActivity)
+//  val vm: HomeViewModel = viewModel()
+  ElevatedCard(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(15.dp),
+  ) {
+    Row(
+      modifier = Modifier
+        .padding(10.dp)
+        .fillMaxWidth(), horizontalArrangement = Arrangement.Center
+    ) {
+      Text(stringResource(id = R.string.stopwatch), fontSize = 20.sp)
+    }
+    Row(
+      modifier = Modifier
+        .padding(10.dp)
+        .fillMaxWidth(),
+      horizontalArrangement = Arrangement.Center,
+      verticalAlignment = Alignment.CenterVertically,
+    ) {
+      // todo
 //      ChangeTimerColorButton("change_color/stopwatch", vm.stopwatchHaloColor)
-//      Spacer(Modifier.width(40.dp))
-//      Button(modifier = Modifier.testTag("stopwatch_create"), onClick = {
-//        logd("start stopwatch")
-//        if (!Settings.canDrawOverlays(context)) {
-//          vm.showGrantOverlayDialog = true
-//          return@Button
-//        }
+      Spacer(Modifier.width(40.dp))
+      Button(modifier = Modifier.testTag("stopwatch_create"), onClick = {
+        logd("start stopwatch")
+        if (!Settings.canDrawOverlays(context)) {
+          sharedVm.showGrantOverlayDialog = true
+          return@Button
+        }
+        // todo
 //        vm.stopwatchButtonClick()
-//      }) {
-//        Text(stringResource(id = R.string.create))
-//      }
-//    }
-//  }
-//}
+      }) {
+        Text(stringResource(id = R.string.create))
+      }
+    }
+  }
+}
---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpStopwatchScreenVm.kt
@@ -0,0 +1,27 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class TmpStopwatchScreenVm(
+  private val application: Application,
+) : AndroidViewModel(application) {
+
+
+  // move premiumVmc to sharedVm
+
+//  fun stopwatchButtonClick() {
+//    viewModelScope.launch {
+//      if (shouldShowPremiumDialog()) {
+//        premiumVmc.showPurchaseDialog = true
+//        return@launch
+//      }
+//      boundFloatingServiceVmc.provideFloatingService().overlayController.addStopwatch(
+//        stopwatchHaloColor
+//      )
+//    }
+//  }
+
+}
