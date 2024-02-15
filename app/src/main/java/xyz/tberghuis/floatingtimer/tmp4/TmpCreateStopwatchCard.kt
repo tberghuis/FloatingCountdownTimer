25257aef8aa5441478383d108e0dcd0c1847e48d
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-16 09:18:02 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpCreateStopwatchCard.kt
@@ -0,0 +1,62 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import android.provider.Settings
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import xyz.tberghuis.floatingtimer.R
+import xyz.tberghuis.floatingtimer.logd
+
+//@Composable
+//fun TmpCreateStopwatchCard() {
+//  val context = LocalContext.current
+//
+////  val vm: HomeViewModel = viewModel()
+//  ElevatedCard(
+//    modifier = Modifier
+//      .fillMaxWidth()
+//      .padding(15.dp),
+//  ) {
+//    Row(
+//      modifier = Modifier
+//        .padding(10.dp)
+//        .fillMaxWidth(), horizontalArrangement = Arrangement.Center
+//    ) {
+//      Text(stringResource(id = R.string.stopwatch), fontSize = 20.sp)
+//    }
+//    Row(
+//      modifier = Modifier
+//        .padding(10.dp)
+//        .fillMaxWidth(),
+//      horizontalArrangement = Arrangement.Center,
+//      verticalAlignment = Alignment.CenterVertically,
+//    ) {
+//      ChangeTimerColorButton("change_color/stopwatch", vm.stopwatchHaloColor)
+//      Spacer(Modifier.width(40.dp))
+//      Button(modifier = Modifier.testTag("stopwatch_create"), onClick = {
+//        logd("start stopwatch")
+//        if (!Settings.canDrawOverlays(context)) {
+//          vm.showGrantOverlayDialog = true
+//          return@Button
+//        }
+//        vm.stopwatchButtonClick()
+//      }) {
+//        Text(stringResource(id = R.string.create))
+//      }
+//    }
+//  }
+//}
