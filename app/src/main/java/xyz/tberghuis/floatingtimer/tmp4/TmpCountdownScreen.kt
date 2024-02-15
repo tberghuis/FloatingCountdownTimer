66bc87ea724bae206cf2051476783600f453e459
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-02-16 09:06:17 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpCountdownScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpCountdownScreen.kt
@@ -1,50 +1,32 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
-import androidx.activity.ComponentActivity
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
-import xyz.tberghuis.floatingtimer.LocalNavController
 import xyz.tberghuis.floatingtimer.R
 import xyz.tberghuis.floatingtimer.composables.PremiumDialog
 import xyz.tberghuis.floatingtimer.logd
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TmpCountdownScreen(
   vm: TmpCountdownScreenVm = viewModel()
 ) {
-  val sharedVm: TmpSharedVm = viewModel(LocalContext.current as ComponentActivity)
   val focusManager = LocalFocusManager.current
-  val navController = LocalNavController.current
-  var showMenu by remember { mutableStateOf(false) }
+
   Scaffold(
     modifier = Modifier.pointerInput(Unit) {
       detectTapGestures(onTap = {
@@ -52,40 +34,7 @@ fun TmpCountdownScreen(
         logd("on tap")
       })
     },
-    topBar = {
-      TopAppBar(
-        title = { Text(stringResource(R.string.app_name)) },
-        modifier = Modifier,
-        actions = {
-          IconButton(onClick = {
-            showMenu = true
-          }) {
-            Icon(Icons.Filled.MoreVert, stringResource(R.string.settings))
-          }
-          DropdownMenu(
-            expanded = showMenu,
-            onDismissRequest = { showMenu = false },
-          ) {
-            DropdownMenuItem(
-              text = { Text(stringResource(R.string.default_color)) },
-              onClick = {
-                navController.navigate("change_color")
-              },
-            )
-            DropdownMenuItem(
-              text = { Text(stringResource(R.string.change_size)) },
-              onClick = { navController.navigate("change_size") },
-            )
-            DropdownMenuItem(
-              text = { Text(stringResource(R.string.cancel_all_timers)) },
-              onClick = {
-                sharedVm.cancelAllTimers()
-              },
-            )
-          }
-        },
-      )
-    },
+    topBar = { TmpFtTopAppBar() },
     bottomBar = {
       TmpBottomBar(TmpScreenTypeCountdown)
     },
---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpFtTopAppBar.kt
@@ -0,0 +1,63 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import xyz.tberghuis.floatingtimer.LocalNavController
+import xyz.tberghuis.floatingtimer.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TmpFtTopAppBar() {
+  val sharedVm: TmpSharedVm = viewModel(LocalContext.current as ComponentActivity)
+  val navController = LocalNavController.current
+  var showMenu by remember { mutableStateOf(false) }
+  TopAppBar(
+    title = { Text(stringResource(R.string.app_name)) },
+    modifier = Modifier,
+    actions = {
+      IconButton(onClick = {
+        showMenu = true
+      }) {
+        Icon(Icons.Filled.MoreVert, stringResource(R.string.settings))
+      }
+      DropdownMenu(
+        expanded = showMenu,
+        onDismissRequest = { showMenu = false },
+      ) {
+        DropdownMenuItem(
+          text = { Text(stringResource(R.string.default_color)) },
+          onClick = {
+            navController.navigate("change_color")
+          },
+        )
+        DropdownMenuItem(
+          text = { Text(stringResource(R.string.change_size)) },
+          onClick = { navController.navigate("change_size") },
+        )
+        DropdownMenuItem(
+          text = { Text(stringResource(R.string.cancel_all_timers)) },
+          onClick = {
+            sharedVm.cancelAllTimers()
+          },
+        )
+      }
+    },
+  )
+}
