5415f6e4bf42914fe55436ae4dc16a03ae5934cb
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-10 13:53:05 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/TmpTimerRect.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/TmpTimerRect.kt
@@ -1,21 +1,15 @@
 package xyz.tberghuis.floatingtimer.tmp2
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import xyz.tberghuis.floatingtimer.common.TimeDisplay
@@ -27,7 +21,6 @@ fun TmpTimerRect(
   vm: TmpVm = viewModel()
 ) {
   // represents window.addView size....
-
 
   Box(
     modifier = Modifier
@@ -43,11 +36,9 @@ fun TmpTimerRect(
     ) {
       Column(
         modifier = Modifier,
-//          .width(vm.settingsTimerPreviewVmc.bubbleSizeDp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
       ) {
-//        Text("00:59")
         TimeDisplay(59, vm.settingsTimerPreviewVmc.fontSize)
 
         Box(
@@ -64,44 +55,7 @@ fun TmpTimerRect(
             vm.settingsTimerPreviewVmc.haloColor
           )
         }
-
-
       }
     }
   }
 }
-
-@Preview(showBackground = true)
-@Composable
-fun PaddingSizeDemo() {
-
-  Column(modifier = Modifier.size(100.dp)) {
-    Box(
-      Modifier
-        .padding(5.dp)
-        .width(50.dp)
-        .background(Color.Green)
-    ) {
-      Text("hello")
-    }
-
-    Box(
-      Modifier
-        .width(50.dp)
-        .padding(5.dp)
-
-        .background(Color.Green)
-    ) {
-      Text("hello")
-    }
-
-
-
-    Box(Modifier) {
-      Text("hello")
-    }
-  }
-
-
-}
-
