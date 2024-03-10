8ac0ec31e7dfe627dc0b0be0f8921a66ffaef9a2
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-10 13:04:13 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
@@ -2,26 +2,34 @@ package xyz.tberghuis.floatingtimer.tmp
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import xyz.tberghuis.floatingtimer.service.countdown.CountdownViewDisplay
 import xyz.tberghuis.floatingtimer.tmp2.TmpTimerRect
 import xyz.tberghuis.floatingtimer.tmp4.CountdownProgressLine
 
 @Composable
 fun TmpScreen(
-//  vm: TmpVm = viewModel()
+  vm: TmpVm = viewModel()
 ) {
+
+
   Column(
     modifier = Modifier.padding(horizontal = 30.dp),
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
-//    Text("tmp screen")
-    TmpTimerRect()
+
+    Row {
+      CountdownViewDisplay(vm.settingsTimerPreviewVmc, 0.6f, 59, false)
+      TmpTimerRect()
+    }
 
 
   }
