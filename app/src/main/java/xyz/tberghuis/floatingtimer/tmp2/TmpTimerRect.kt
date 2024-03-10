20c4387b70729ceffeaed02152e32b46ee3b31d2
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-10 15:33:08 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/TmpTimerRect.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp2/TmpTimerRect.kt
@@ -1,8 +1,11 @@
 package xyz.tberghuis.floatingtimer.tmp2
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,6 +13,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import xyz.tberghuis.floatingtimer.common.TimeDisplay
@@ -25,6 +29,7 @@ fun TmpTimerRect(
   Box(
     modifier = Modifier
       .width(vm.settingsTimerPreviewVmc.bubbleSizeDp)
+//      .height(vm.settingsTimerPreviewVmc.bubbleSizeDp * .6f)
       .padding(5.dp),
     contentAlignment = Alignment.Center,
   ) {
@@ -35,7 +40,7 @@ fun TmpTimerRect(
       shadowElevation = 5.dp,
     ) {
       Column(
-        modifier = Modifier,
+        modifier = Modifier.background(Color.White),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
       ) {
