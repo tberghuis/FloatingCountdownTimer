9af53f9af6a4784316ade498e304b516b759955a
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-13 12:34:27 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpRadioButtonDemo.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpRadioButtonDemo.kt
@@ -1,13 +1,38 @@
 package xyz.tberghuis.floatingtimer.tmp6
 
+import android.app.Application
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+class TmpRadioButtonVm(private val application: Application) : AndroidViewModel(application) {
+
+}
 
 @Composable
-fun TmpRadioButtonDemo() {
+fun TmpRadioButtonDemo(
+  vm: TmpRadioButtonVm = viewModel()
+) {
   Text("hello radio button")
 
-  RadioButton()
+  Row {
+    RadioButton(
+      selected = true,
+      onClick = {},
+      modifier = Modifier,
+    )
+    Text("circle")
+    RadioButton(
+      selected = false,
+      onClick = {},
+      modifier = Modifier,
+    )
+    Text("rectangle")
+  }
+
 
 }
