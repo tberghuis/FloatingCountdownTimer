774fd421fe9bc09b64fb03ea23dda9ba24623822
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-13 12:21:39 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpRadioButtonDemo.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpRadioButtonDemo.kt
@@ -1,9 +1,13 @@
 package xyz.tberghuis.floatingtimer.tmp6
 
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun TmpRadioButtonDemo() {
   Text("hello radio button")
+
+  RadioButton()
+
 }
