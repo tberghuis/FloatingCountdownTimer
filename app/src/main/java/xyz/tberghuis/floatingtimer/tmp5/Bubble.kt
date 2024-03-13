747fe99990df2e907287b66a4495275e42bf5167
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-13 09:57:23 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Bubble.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Bubble.kt
@@ -3,6 +3,7 @@ package xyz.tberghuis.floatingtimer.tmp5
 import android.util.Log
 import androidx.compose.ui.graphics.Color
 import xyz.tberghuis.floatingtimer.service.FloatingService
+import kotlin.math.roundToInt
 
 abstract class Bubble(
   private val service: FloatingService,
@@ -27,11 +28,8 @@ abstract class Bubble(
     }
   }
 
-
-//  val bubbleSizePx: Int = (bubbleSizeDp.value * service.resources.displayMetrics.density).toInt()
-
-  val widthPx: Int = (widthDp.value * service.resources.displayMetrics.density).toInt()
-  val heightPx: Int = (heightDp.value * service.resources.displayMetrics.density).toInt()
+  val widthPx: Int = (widthDp.value * service.resources.displayMetrics.density).roundToInt()
+  val heightPx: Int = (heightDp.value * service.resources.displayMetrics.density).roundToInt()
 
   override val arcWidth = BubbleProperties.calcArcWidth(bubbleSizeScaleFactor)
   override val fontSize = BubbleProperties.calcFontSize(bubbleSizeScaleFactor)
