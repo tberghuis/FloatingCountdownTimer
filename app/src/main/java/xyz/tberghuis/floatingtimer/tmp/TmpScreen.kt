cf634c4ac4705859233a016d4bbb68b9bea19299
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-15 11:30:19 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
@@ -7,27 +7,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import xyz.tberghuis.floatingtimer.tmp6.TmpRadioButtonDemo
 
 @Composable
 fun TmpScreen(
-  vm: TmpVm = viewModel()
+//  vm: TmpVm = viewModel()
 ) {
-
-
   Column(
     modifier = Modifier.padding(horizontal = 30.dp),
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
-
-//    Row {
-//      TmpTimerRect()
-//    }
-//    TmpSliderScale(vm.settingsTimerPreviewVmc)
-
-    TmpRadioButtonDemo()
 
   }
 }
