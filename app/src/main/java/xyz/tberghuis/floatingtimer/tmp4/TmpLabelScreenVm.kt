baed0d5ec717eb8202cb21430026600adedebc6f
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-22 19:05:18 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpLabelScreenVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpLabelScreenVm.kt
@@ -9,9 +9,11 @@ import android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,13 +24,13 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.setViewTreeLifecycleOwner
 import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import kotlinx.coroutines.delay
 import xyz.tberghuis.floatingtimer.logd
 import kotlin.math.roundToInt
 
 class TmpLabelScreenVm(
   private val application: Application,
 ) : AndroidViewModel(application) {
-  val sizePx: Int = 60.dpToPx(application)
 
 //  val params = WindowManager.LayoutParams(
 //    sizePx,
@@ -56,7 +58,7 @@ class TmpLabelScreenVm(
 
   fun createOverlay(activity: ComponentActivity) {
     logd("createOverlay")
-    view = createView(activity)
+    view = createView(activity, ::updateSize)
 
     params.gravity = Gravity.START or Gravity.TOP
 
@@ -69,8 +71,8 @@ class TmpLabelScreenVm(
 //    params.x = 120.dpToPx(application)
 //    params.y = 120.dpToPx(application)
 
-    params.width = 120.dpToPx(application)
-    params.height = 120.dpToPx(application)
+    params.width = 60.dpToPx(application)
+    params.height = 60.dpToPx(application)
 
 
     windowManager.updateViewLayout(view, params)
@@ -81,29 +83,43 @@ fun Int.dpToPx(context: Context): Int {
   return (this * context.resources.displayMetrics.density).roundToInt()
 }
 
-fun createView(activity: ComponentActivity): ComposeView {
+fun createView(activity: ComponentActivity, updateSize: () -> Unit): ComposeView {
   return ComposeView(activity).apply {
     setContent {
-      Box(
-        modifier = Modifier
-          .requiredSize(120.dp)
-          .background(Color.Red)
-          .onGloballyPositioned {
-            logd("outer box layout coords ${it.size}")
 
-          },
-        contentAlignment = Alignment.TopStart,
+      LaunchedEffect(Unit) {
+        delay(5000)
+        updateSize()
+      }
+
+
+
+      Row(
+        modifier = Modifier.onGloballyPositioned {
+          logd("row layout coords ${it.size}")
+        },
       ) {
-        Box(
-          modifier = Modifier
-            .size(60.dp)
-            .background(Color.Green)
-            .onGloballyPositioned {
-              logd("inner box layout coords ${it.size}")
-            },
-          contentAlignment = Alignment.TopStart,
+        Row(
+          modifier = Modifier.onGloballyPositioned {
+            logd("inner row layout coords ${it.size}")
+          },
         ) {
-          Text("hello overlay")
+          Box(
+            modifier = Modifier
+              .size(60.dp)
+              .background(Color.Red),
+            contentAlignment = Alignment.TopStart,
+          ) {
+            Text("red")
+          }
+          Box(
+            modifier = Modifier
+              .size(60.dp)
+              .background(Color.Green),
+            contentAlignment = Alignment.TopStart,
+          ) {
+            Text("green")
+          }
         }
       }
     }
