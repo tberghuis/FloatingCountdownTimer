715a16c7d1e17551882748e60dfc59eddd5f9fd7
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-22 17:45:53 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpLabelScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpLabelScreen.kt
@@ -1,5 +1,6 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -8,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -15,13 +17,16 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 fun TmpLabelScreen(
   vm: TmpLabelScreenVm = viewModel()
 ) {
+
+  val context = LocalContext.current
+
   Column(
     modifier = Modifier.padding(horizontal = 30.dp),
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
     Text("hello label screen")
-    Button(onClick = { vm.createOverlay() }) {
+    Button(onClick = { vm.createOverlay(context as ComponentActivity) }) {
       Text("create overlay")
     }
   }
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpLabelScreenVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpLabelScreenVm.kt
@@ -1,15 +1,57 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
 import android.app.Application
+import android.content.Context
+import android.graphics.PixelFormat
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 import xyz.tberghuis.floatingtimer.logd
+import kotlin.math.roundToInt
 
 class TmpLabelScreenVm(
   private val application: Application,
 //  private val state: SavedStateHandle
 ) : AndroidViewModel(application) {
-  fun createOverlay() {
+  fun createOverlay(activity: ComponentActivity) {
     logd("createOverlay")
+
+    val windowManager = application.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+    val overlaySize = 60.dp
+
+    val sizePx: Int =
+      (overlaySize.value * application.resources.displayMetrics.density).roundToInt()
+
+    val params = WindowManager.LayoutParams(
+      sizePx,
+      sizePx,
+      0,
+      0,
+      WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+      WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+      PixelFormat.TRANSLUCENT
+    )
+
+    val view = ComposeView(activity).apply {
+      setContent {
+        Text("hello overlay")
+      }
+
+      setViewTreeLifecycleOwner(activity)
+      setViewTreeSavedStateRegistryOwner(activity)
+//propagateViewTreeSavedStateRegistryOwner
+    }
+
+
+    windowManager.addView(view, params)
+
+
   }
 
 }
