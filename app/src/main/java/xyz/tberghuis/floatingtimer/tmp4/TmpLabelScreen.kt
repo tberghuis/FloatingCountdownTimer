457791c4e35ae2fc4fc0251f6289d2f19415089d
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-24 07:24:38 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpLabelScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpLabelScreen.kt
@@ -1,40 +1,30 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.first
 import xyz.tberghuis.floatingtimer.logd
 
 @Composable
 fun TmpLabelScreen(
   vm: TmpLabelScreenVm = viewModel()
 ) {
-
   val context = LocalContext.current
-
   Column(
     modifier = Modifier.padding(horizontal = 30.dp),
     verticalArrangement = Arrangement.Center,
@@ -44,19 +34,11 @@ fun TmpLabelScreen(
     Button(onClick = { vm.createOverlay(context as ComponentActivity) }) {
       Text("create overlay")
     }
-
-
-    Button(onClick = { vm.updateSize() }) {
-      Text("update size")
-    }
-
-
     Button(onClick = {
       logd("press me")
     }) {
       Text("press me")
     }
-
   }
 }
 
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpLabelScreenVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpLabelScreenVm.kt
@@ -6,36 +6,13 @@ import android.graphics.PixelFormat
 import android.view.Gravity
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.setViewTreeLifecycleOwner
-import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 import xyz.tberghuis.floatingtimer.logd
-import kotlin.math.roundToInt
 
 class TmpLabelScreenVm(
   private val application: Application,
 ) : AndroidViewModel(application) {
-
-//  val params = WindowManager.LayoutParams(
-//    sizePx,
-//    sizePx,
-//    0,
-//    0,
-//    WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-//    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-//    PixelFormat.TRANSLUCENT
-//  )
 
   val params = WindowManager.LayoutParams(
     WindowManager.LayoutParams.MATCH_PARENT,
@@ -57,58 +34,7 @@ class TmpLabelScreenVm(
 
   fun createOverlay(activity: ComponentActivity) {
     logd("createOverlay")
-//    view = createView(activity, ::updateSize)
     view = createViewRow(activity, params)
     windowManager.addView(view, params)
-  }
-
-  fun updateSize() {
-    params.width = 60.dpToPx(application)
-    params.height = 60.dpToPx(application)
-    windowManager.updateViewLayout(view, params)
-  }
-}
-
-fun Int.dpToPx(context: Context): Int {
-  return (this * context.resources.displayMetrics.density).roundToInt()
-}
-
-fun createView(activity: ComponentActivity, updateSize: () -> Unit): ComposeView {
-  return ComposeView(activity).apply {
-    setContent {
-      Row(
-        modifier = Modifier
-          .graphicsLayer {
-            this.alpha = .5f
-          },
-      ) {
-        Row(
-          modifier = Modifier
-            .runOnceOnGloballyPositioned {
-              logd("runOnceOnGloballyPositioned")
-              updateSize()
-            },
-        ) {
-          Box(
-            modifier = Modifier
-              .size(60.dp)
-              .background(Color.Red),
-            contentAlignment = Alignment.TopStart,
-          ) {
-            Text("red")
-          }
-          Box(
-            modifier = Modifier
-              .size(60.dp)
-              .background(Color.Green),
-            contentAlignment = Alignment.TopStart,
-          ) {
-            Text("green")
-          }
-        }
-      }
-    }
-    setViewTreeLifecycleOwner(activity)
-    setViewTreeSavedStateRegistryOwner(activity)
   }
 }
