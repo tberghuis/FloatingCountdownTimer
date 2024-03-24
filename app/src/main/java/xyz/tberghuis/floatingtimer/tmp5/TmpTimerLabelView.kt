357ada2f24027bf9102d69ab16f27d1df93bd690
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-24 14:25:28 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/TmpTimerLabelView.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/TmpTimerLabelView.kt
@@ -5,25 +5,25 @@ import android.view.WindowManager
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,33 +39,6 @@ import xyz.tberghuis.floatingtimer.composables.CountdownProgressLine
 import xyz.tberghuis.floatingtimer.logd
 import xyz.tberghuis.floatingtimer.tmp4.LocalTimerViewHolder
 import xyz.tberghuis.floatingtimer.tmp4.runOnceOnGloballyPositioned
-
-//@Composable
-//fun TmpTimerLabelView() {
-//  val tvh = LocalTimerViewHolder.current
-//  val windowManager = tvh.service.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-//
-//  Box {
-//    Row(modifier = Modifier
-//      .runOnceOnGloballyPositioned {
-//        logd("runOnceOnGloballyPositioned ${it.size}")
-//        tvh.params.width = it.size.width
-//        tvh.params.height = it.size.height
-//        windowManager.updateViewLayout(tvh.view, tvh.params)
-//      }
-//      .border(1.dp, Color.Black))
-//    {
-//      Text("this is label")
-//      Box(
-//        modifier = Modifier
-//          .size(60.dp)
-//          .background(Color.Green)
-//      ) {
-//        Text("00:59")
-//      }
-//    }
-//  }
-//}
 
 // hardcoded from scratch
 // TimerRectView as reference
@@ -92,8 +65,7 @@ fun TmpTimerLabelView(
 //    label
 //  )
 
-
-  val isPaused = false
+  val isPaused = true
   val arcWidth = ARC_WIDTH_NO_SCALE * (0.9f * 1f + 1)
   val haloColor = Color.Green
   val timeElapsed = 59
@@ -101,10 +73,8 @@ fun TmpTimerLabelView(
   val fontSize = TIMER_FONT_SIZE_NO_SCALE * (1.2 * 1f + 1)
   val label = "label"
 
-
   val tvh = LocalTimerViewHolder.current
   val windowManager = tvh.service.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-
 
   TimerLabelView(
     isPaused,
@@ -141,16 +111,27 @@ fun TimerLabelView(
         .runOnceOnGloballyPositioned {
           updateViewLayout?.invoke(it.size)
         }
+        .height(IntrinsicSize.Min)
+        .width(IntrinsicSize.Min)
         .padding(5.dp)
         .graphicsLayer(
           shadowElevation = with(LocalDensity.current) { 5.dp.toPx() },
           shape = RoundedCornerShape(10.dp),
           clip = true
-        ),
+        )
+        .background(Color.White),
+      contentAlignment = Alignment.Center,
     ) {
+      if (isPaused) {
+        Icon(
+          Icons.Filled.PlayArrow,
+          contentDescription = "paused",
+          modifier = Modifier.fillMaxSize(),
+          tint = Color.LightGray
+        )
+      }
       Column(
         modifier = Modifier
-          .background(Color.White)
           .width(IntrinsicSize.Max)
           .padding(5.dp)
       ) {
