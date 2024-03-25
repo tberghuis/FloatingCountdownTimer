fd4390f86630aa71abdba6072836c66f348639bd
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-26 07:47:58 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpTimerShapeChoice.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpTimerShapeChoice.kt
@@ -14,9 +14,14 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import xyz.tberghuis.floatingtimer.R
 import xyz.tberghuis.floatingtimer.viewmodels.TimerShapeChoiceVm
@@ -72,3 +77,21 @@ fun ColumnScope.TmpTimerShapeChoice(vm: TimerShapeChoiceVm) {
     }
   }
 }
+
+@Preview
+@Composable
+fun PreviewTmpTimerShapeChoice() {
+
+  val vm = remember {
+    object : TimerShapeChoiceVm {
+      override var timerShape by mutableStateOf("circle")
+      override var label by mutableStateOf("")
+    }
+  }
+
+  Column {
+    TmpTimerShapeChoice(vm)
+  }
+}
+
+
