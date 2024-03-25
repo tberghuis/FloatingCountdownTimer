86e599d4c76f501329602aea03a48a36b5e786ee
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-25 12:42:51 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpTimerShapeChoice.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpTimerShapeChoice.kt
@@ -1,8 +1,11 @@
 package xyz.tberghuis.floatingtimer.tmp6
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Circle
 import androidx.compose.material.icons.outlined.Rectangle
@@ -21,44 +24,51 @@ import xyz.tberghuis.floatingtimer.viewmodels.TimerShapeChoiceVm
 @Composable
 fun ColumnScope.TmpTimerShapeChoice(vm: TimerShapeChoiceVm) {
   Row(
-    modifier = Modifier.align(Alignment.CenterHorizontally),
+    modifier = Modifier
+      .align(Alignment.CenterHorizontally)
+      .padding(10.dp),
 //    horizontalArrangement = Arrangement.Center,
     verticalAlignment = Alignment.CenterVertically,
   ) {
     Text(stringResource(R.string.shape_label))
-    RadioButton(
-      selected = vm.timerShape == "circle",
-      onClick = { vm.timerShape = "circle" },
-      modifier = Modifier,
-    )
-    Icon(
-      Icons.Outlined.Circle,
-      contentDescription = stringResource(R.string.circle),
-      modifier = Modifier.size(40.dp)
-    )
+    Column {
+      Row {
+        RadioButton(
+          selected = vm.timerShape == "circle",
+          onClick = { vm.timerShape = "circle" },
+          modifier = Modifier,
+        )
+        Icon(
+          Icons.Outlined.Circle,
+          contentDescription = stringResource(R.string.circle),
+          modifier = Modifier.size(40.dp)
+        )
 
-    RadioButton(
-      selected = vm.timerShape == "rectangle",
-      onClick = { vm.timerShape = "rectangle" },
-      modifier = Modifier,
-    )
-    Icon(
-      Icons.Outlined.Rectangle,
-      contentDescription = stringResource(R.string.rectangle),
-      modifier = Modifier.size(40.dp)
-    )
-
-    RadioButton(
-      selected = vm.timerShape == "label",
-      onClick = { vm.timerShape = "label" },
-      modifier = Modifier,
-    )
-    Text(stringResource(R.string.label))
+        RadioButton(
+          selected = vm.timerShape == "rectangle",
+          onClick = { vm.timerShape = "rectangle" },
+          modifier = Modifier,
+        )
+        Icon(
+          Icons.Outlined.Rectangle,
+          contentDescription = stringResource(R.string.rectangle),
+          modifier = Modifier.size(40.dp)
+        )
+      }
+      Row {
+        RadioButton(
+          selected = vm.timerShape == "label",
+          onClick = { vm.timerShape = "label" },
+          modifier = Modifier,
+        )
+        TextField(
+          value = vm.label,
+          onValueChange = { vm.label = it },
+          modifier = Modifier.widthIn(max = 200.dp),
+          enabled = true,
+          label = { Text(stringResource(R.string.label)) }
+        )
+      }
+    }
   }
-
-  TextField(
-    value = vm.label,
-    onValueChange = { vm.label = it },
-    label = { Text(stringResource(R.string.label)) }
-  )
 }
