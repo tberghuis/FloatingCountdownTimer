96df3208293174e811be4c7686e15fe444e8c90a
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-25 10:31:12 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/TmpTimerViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package xyz.tberghuis.floatingtimer.tmp5
-
-import android.graphics.PixelFormat
-import android.view.Gravity
-import android.view.WindowManager
-import xyz.tberghuis.floatingtimer.logd
-import xyz.tberghuis.floatingtimer.service.FloatingService
-import xyz.tberghuis.floatingtimer.service.createComposeView
-
