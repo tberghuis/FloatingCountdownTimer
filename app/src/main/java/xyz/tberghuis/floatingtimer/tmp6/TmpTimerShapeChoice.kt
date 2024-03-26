9d6a3e776ceae74f95b8aaddba897f0137b017a4
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-03-26 08:31:22 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpTimerShapeChoice.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpTimerShapeChoice.kt
@@ -1,8 +1,11 @@
 package xyz.tberghuis.floatingtimer.tmp6
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
@@ -14,20 +17,33 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import xyz.tberghuis.floatingtimer.R
+import xyz.tberghuis.floatingtimer.logd
 import xyz.tberghuis.floatingtimer.viewmodels.TimerShapeChoiceVm
 
 @Composable
 fun ColumnScope.TmpTimerShapeChoice(vm: TimerShapeChoiceVm) {
+  val focusRequester = remember { FocusRequester() }
+  if (vm.timerShape == "label") {
+    LaunchedEffect(Unit) {
+      focusRequester.requestFocus()
+    }
+  }
+
   Row(
     modifier = Modifier
       .align(Alignment.CenterHorizontally)
@@ -62,14 +78,22 @@ fun ColumnScope.TmpTimerShapeChoice(vm: TimerShapeChoiceVm) {
       Row {
         RadioButton(
           selected = vm.timerShape == "label",
-          onClick = { vm.timerShape = "label" },
+          onClick = {
+            logd("radiobutton clickable")
+            vm.timerShape = "label"
+          },
           modifier = Modifier,
         )
         TextField(
           value = vm.label,
           onValueChange = { vm.label = it },
-          modifier = Modifier.widthIn(max = 200.dp),
-          enabled = true,
+          modifier = Modifier
+            .widthIn(max = 200.dp)
+            .focusRequester(focusRequester)
+            .clickable {
+              vm.timerShape = "label"
+            },
+          enabled = vm.timerShape == "label",
           label = { Text(stringResource(R.string.label)) }
         )
       }
@@ -80,17 +104,21 @@ fun ColumnScope.TmpTimerShapeChoice(vm: TimerShapeChoiceVm) {
 @Preview
 @Composable
 fun PreviewTmpTimerShapeChoice() {
-
   val vm = remember {
     object : TimerShapeChoiceVm {
       override var timerShape by mutableStateOf("circle")
       override var label by mutableStateOf("")
     }
   }
-
-  Column {
+  val localFocusManager = LocalFocusManager.current
+  Column(
+    Modifier
+      .fillMaxSize()
+      .pointerInput(Unit) {
+        detectTapGestures(onTap = {
+          localFocusManager.clearFocus()
+        })
+      }) {
     TmpTimerShapeChoice(vm)
   }
 }
-
-
