2a7a028da0e9264b34b69447cc9d8a883895d189
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-04-04 17:19:26 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpTextShadow.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpTextShadow.kt
@@ -1,13 +1,25 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
@@ -17,7 +29,51 @@ fun TmpTextShadow() {
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
-    Text("hello text shadow 123")
-
+    Box {
+      Box(
+        Modifier
+          .background(Color.Red)
+          .size(100.dp)
+      )
+      TextShadow()
+      TextShadowWhite()
+    }
   }
 }
+
+@Composable
+fun TextShadow() {
+  Text(
+    text = "Sample",
+    style = TextStyle.Default.copy(
+      fontSize = 64.sp,
+      drawStyle = Stroke(
+        miter = 10f,
+        width = 5f,
+        join = StrokeJoin.Round
+      ),
+
+      shadow = Shadow(
+        color = Color.Gray,
+        offset = Offset(-5f, 5f),
+        blurRadius = 8f
+      )
+    )
+  )
+}
+
+@Composable
+fun TextShadowWhite() {
+  Text(
+    text = "Sample",
+    style = TextStyle.Default.copy(
+      color = Color.White,
+      fontSize = 64.sp,
+      drawStyle = Fill
+    )
+  )
+}
+
+
+
+
