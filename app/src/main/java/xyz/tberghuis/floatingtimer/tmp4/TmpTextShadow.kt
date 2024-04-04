704218c11165120fbc133ecda22ec985afac1e26
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-04-04 17:46:20 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpTextShadow.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpTextShadow.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -16,11 +17,14 @@ import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import xyz.tberghuis.floatingtimer.TIMER_FONT_SIZE_NO_SCALE
 
 @Preview
 @Composable
@@ -37,8 +41,15 @@ fun TmpTextShadow() {
           .size(200.dp)
       )
       Column {
-        OutlinedTextWithShadow("00:59")
-        OutlinedTextWithShadow("00:59")
+        CompositionLocalProvider(
+          LocalDensity provides Density(LocalDensity.current.density, 1f)
+        ) {
+          OutlinedTextWithShadow("00:59", 16.sp)
+          // 35.2
+          OutlinedTextWithShadow("00:59", TIMER_FONT_SIZE_NO_SCALE * (1.2 * 1f + 1))
+        }
+
+
       }
 
     }
@@ -96,11 +107,13 @@ fun OutlinedTextWithShadow(
           width = 5f,
           join = StrokeJoin.Round
         ),
-        shadow = Shadow(
-          color = Color.Gray,
-          offset = Offset(-5f, 5f),
-          blurRadius = 8f
-        )
+        // stuff shadow as would effect performance
+        // and I would need to calc based on current scale
+//        shadow = Shadow(
+//          color = Color.Gray,
+//          offset = Offset(-5f, 5f),
+//          blurRadius = 8f
+//        )
       )
     )
 
