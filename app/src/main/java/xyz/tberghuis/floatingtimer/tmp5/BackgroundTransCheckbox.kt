fc551ab8b4b9d0ec83909448e7f3c52d83814f7d
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-04-09 11:34:46 +0800

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/BackgroundTransCheckbox.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/BackgroundTransCheckbox.kt
@@ -4,14 +4,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 interface BackgroundTransCheckboxVm {
   var isBackgroundTransparent: Boolean
@@ -19,7 +22,12 @@ interface BackgroundTransCheckboxVm {
 
 @Composable
 fun ColumnScope.BackgroundTransCheckbox(vm: BackgroundTransCheckboxVm) {
-  Row {
+  Row(
+    modifier = Modifier
+      .align(Alignment.CenterHorizontally)
+      .padding(10.dp),
+    verticalAlignment = Alignment.CenterVertically,
+  ) {
     Text("checkbox")
 
   }
