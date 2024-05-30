719fa209ea6c3633d0e80a77ddc09f54da0a54fb
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-05-30 13:06:11 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpRingtone.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpRingtone.kt
@@ -1,5 +1,10 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
+import android.content.Intent
+import android.media.RingtoneManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -10,11 +15,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import xyz.tberghuis.floatingtimer.logd
 
 @Composable
 fun TmpRingtone(
   vm: TmpRingtoneVm = viewModel(),
 ) {
+
+
   Column(
     modifier = Modifier.padding(horizontal = 30.dp),
     verticalArrangement = Arrangement.Center,
@@ -23,6 +31,7 @@ fun TmpRingtone(
     Text("hello ringtone")
     Button(onClick = {
       vm.getRingtoneList()
+
     }) {
       Text("button")
     }
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpRingtoneVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpRingtoneVm.kt
@@ -1,11 +1,28 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
 import android.app.Application
+import android.media.RingtoneManager
+import android.media.RingtoneManager.TYPE_ALARM
 import androidx.lifecycle.AndroidViewModel
 import xyz.tberghuis.floatingtimer.logd
 
 class TmpRingtoneVm(private val application: Application) : AndroidViewModel(application) {
   fun getRingtoneList() {
     logd("getRingtoneList")
+
+//    do not use this ACTION_RINGTONE_PICKER
+
+    val manager = RingtoneManager(application)
+    manager.setType(TYPE_ALARM)
+    val cursor = manager.cursor
+    val count = cursor.count
+    if (count > 0 && cursor.moveToFirst()) {
+      do {
+        val titleColumnIndex = cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX)
+        val uriColumnIndex = cursor.getString(RingtoneManager.URI_COLUMN_INDEX)
+        val idColumnIndex = cursor.getString(RingtoneManager.ID_COLUMN_INDEX)
+        logd("titleColumnIndex $titleColumnIndex uriColumnIndex $uriColumnIndex idColumnIndex $idColumnIndex")
+      } while (cursor.moveToNext())
+    }
   }
 }
