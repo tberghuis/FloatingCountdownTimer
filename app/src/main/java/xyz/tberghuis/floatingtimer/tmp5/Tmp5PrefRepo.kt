30f8fc8d6289a1344e402238ad05f473e6077cec
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-05-31 14:44:20 +1000

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5PrefRepo.kt
@@ -0,0 +1,22 @@
+package xyz.tberghuis.floatingtimer.tmp5
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class Tmp5PrefRepo(private val dataStore: DataStore<Preferences>) {
+  val alarmRingtoneUriFlow: Flow<String> = dataStore.data.map { preferences ->
+    // todo get default alarm uri
+    preferences[stringPreferencesKey("alarm_ringtone_uri")]
+      ?: "content://settings/system/alarm_alert"
+  }
+
+  suspend fun updateAlarmRingtoneUri(uri: String) {
+    dataStore.edit { preferences ->
+      preferences[stringPreferencesKey("alarm_ringtone_uri")] = uri
+    }
+  }
+}
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneVm.kt
@@ -1,4 +1,12 @@
 package xyz.tberghuis.floatingtimer.tmp5
 
-class Tmp5RingtoneVm {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import xyz.tberghuis.floatingtimer.data.dataStore
+
+class Tmp5RingtoneVm(private val application: Application) : AndroidViewModel(application) {
+  // todo get singleton from MainApplication
+  private val prefRepo = Tmp5PrefRepo(application.dataStore)
+  
+
 }
