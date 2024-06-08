7cae51b97c3fe328c663f8dad0c5465301d8c652
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-06-08 14:12:54 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneScreen.kt
@@ -2,12 +2,16 @@ package xyz.tberghuis.floatingtimer.tmp5
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import xyz.tberghuis.floatingtimer.LocalNavController
 
@@ -33,8 +37,18 @@ fun Tmp5RingtoneScreen() {
 }
 
 @Composable
-fun Tmp5RingtoneScreenContent(padding: PaddingValues) {
+fun Tmp5RingtoneScreenContent(
+  padding: PaddingValues,
+  vm: Tmp5RingtoneVm = viewModel(),
+) {
+
+  val currentRingtoneTitle by vm.ringtoneTitleFlow.collectAsState(initial = "")
+
   Column(modifier = Modifier.padding(padding)) {
     Text("hello ringtone screen")
+    Row {
+      Text("current")
+      Text(currentRingtoneTitle)
+    }
   }
 }
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneVm.kt
@@ -1,12 +1,30 @@
 package xyz.tberghuis.floatingtimer.tmp5
 
 import android.app.Application
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.flow.map
 import xyz.tberghuis.floatingtimer.data.dataStore
 
 class Tmp5RingtoneVm(private val application: Application) : AndroidViewModel(application) {
   // todo get singleton from MainApplication
   private val prefRepo = Tmp5PrefRepo(application.dataStore)
 
+  var previewRingtone: Ringtone? = null
 
+  // shared flow, preview uri...
+  // if playing same uri just stop
+
+  val ringtoneTitleFlow = prefRepo.alarmRingtoneUriFlow.map {
+    ringtoneTitleFromUri(application, it)
+  }
+
+}
+
+// doitwrong
+fun ringtoneTitleFromUri(application: Application, uri: String): String {
+  val r = RingtoneManager.getRingtone(application, Uri.parse(uri))
+  return r?.getTitle(application) ?: "error: no title"
 }
