45623fe3b4cab367722683d2c7eaa09291fc86df
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-06-09 16:02:42 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneScreen.kt
@@ -1,9 +1,5 @@
 package xyz.tberghuis.floatingtimer.tmp5
 
-import android.content.Context
-import android.media.Ringtone
-import android.media.RingtoneManager
-import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,19 +9,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 import xyz.tberghuis.floatingtimer.LocalNavController
-
 
 @Composable
 fun TmpNavHost() {
@@ -34,7 +21,6 @@ fun TmpNavHost() {
     Tmp5RingtoneScreen()
   }
 }
-
 
 @Composable
 fun Tmp5RingtoneScreen() {
@@ -52,21 +38,11 @@ fun Tmp5RingtoneScreenContent(
   padding: PaddingValues,
   vm: Tmp5RingtoneVm = viewModel(),
 ) {
-  val context = LocalContext.current
-//  val currentRingtoneUri = vm.currentRingtoneUri.collectAsState()
-
-//  val currentRingtone = remember {
-//    derivedStateOf {
-//      currentRingtoneUri.value?.let { ringtoneFromUri(context, it) }
-//    }
-//  }.value
-
   Column(modifier = Modifier.padding(padding)) {
     Text("hello ringtone screen")
     Row {
       // todo outlined text field
       Text("current: ")
-
       Text(
         vm.currentRingtoneVmc.currentRingtoneTitle,
         modifier = Modifier.clickable {
@@ -75,18 +51,6 @@ fun Tmp5RingtoneScreenContent(
           }
         },
       )
-
-
-//      currentRingtone?.let {
-//        Text(
-//          it.getTitle(context),
-//          modifier = Modifier.clickable {
-//            currentRingtoneUri.value?.let { uri ->
-//              vm.ringtonePreviewVmc.ringtoneClick(uri)
-//            }
-//          },
-//        )
-//      }
     }
   }
 }
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneVm.kt
@@ -1,35 +1,16 @@
 package xyz.tberghuis.floatingtimer.tmp5
 
 import android.app.Application
-import android.media.Ringtone
-import android.media.RingtoneManager
-import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import xyz.tberghuis.floatingtimer.data.dataStore
-import xyz.tberghuis.floatingtimer.logd
 
 class Tmp5RingtoneVm(private val application: Application) : AndroidViewModel(application) {
   // todo get singleton from MainApplication
   private val prefRepo = Tmp5PrefRepo(application.dataStore)
-  val currentRingtoneUri = MutableStateFlow<String?>(null)
-
   val ringtonePreviewVmc = TmpRingtonePreviewVmc(application)
-
   val currentRingtoneVmc =
     TmpCurrentRingtoneVmc(prefRepo.alarmRingtoneUriFlow, viewModelScope, application)
-
   init {
-    viewModelScope.launch {
-      prefRepo.alarmRingtoneUriFlow.collect {
-        currentRingtoneUri.value = it
-      }
-    }
   }
 }
