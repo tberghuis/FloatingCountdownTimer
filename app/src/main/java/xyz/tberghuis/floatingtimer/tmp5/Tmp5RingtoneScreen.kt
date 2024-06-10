1703da88143ccf9ac0a12c77f4edb055ef86d18e
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-06-10 11:01:14 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneScreen.kt
@@ -7,13 +7,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import xyz.tberghuis.floatingtimer.LocalNavController
@@ -37,6 +44,7 @@ fun Tmp5RingtoneScreen() {
   }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Tmp5RingtoneScreenContent(
   padding: PaddingValues,
@@ -44,18 +52,47 @@ fun Tmp5RingtoneScreenContent(
 ) {
   LazyColumn(modifier = Modifier.padding(padding)) {
     item {
-      Row {
-        // todo outlined text field
-        Text("current: ")
-        Text(
-          vm.currentRingtoneVmc.currentRingtoneTitle,
-          modifier = Modifier.clickable {
-            vm.currentRingtoneVmc.currentRingtoneUri?.let {
-              vm.ringtonePreviewVmc.ringtoneClick(it)
-            }
-          },
+//      Row {
+//        // todo outlined text field
+//        Text("current: ")
+//        Text(
+//          vm.currentRingtoneVmc.currentRingtoneTitle,
+//          modifier = Modifier.clickable {
+//            vm.currentRingtoneVmc.currentRingtoneUri?.let {
+//              vm.ringtonePreviewVmc.ringtoneClick(it)
+//            }
+//          },
+//        )
+//      }
+
+
+      OutlinedTextField(
+        value = vm.currentRingtoneVmc.currentRingtoneTitle,
+        onValueChange = {},
+        modifier = Modifier.clickable {
+          vm.currentRingtoneVmc.currentRingtoneUri?.let {
+            vm.ringtonePreviewVmc.ringtoneClick(it)
+          }
+        },
+        enabled = false,
+        readOnly = true,
+        label = {
+          Text("current")
+        },
+
+        colors = OutlinedTextFieldDefaults.colors(
+          disabledTextColor = MaterialTheme.colorScheme.onSurface,
+          disabledContainerColor = Color.Transparent,
+          disabledBorderColor = MaterialTheme.colorScheme.outline,
+          disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant,
         )
-      }
+
+
+      )
+
+
+
+
     }
     item {
       HorizontalDivider()
