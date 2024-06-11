c689ea39b6c141796ec8ce2fd3b546b6b267fdae
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-06-11 13:46:52 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpColumnScroll.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/TmpColumnScroll.kt
@@ -2,15 +2,11 @@ package xyz.tberghuis.floatingtimer.tmp6
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,52 +17,28 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
 
-
 @Composable
 fun TmpColumnScroll() {
-
   val widthConstraint = Modifier
     .widthIn(0.dp, 232.dp)
     .fillMaxWidth()
-
 
   LazyColumn(
     modifier = Modifier.background(Color.Cyan),
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
     items(100) { index ->
-
-
       Row(
         modifier = Modifier
           .background(Color.LightGray)
           .then(widthConstraint),
-//          .widthIn(0.dp, 232.dp)
-//          .fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
       ) {
         Text("start line $index")
         Text("end line $index")
       }
-
-
-//      Box(
-//        modifier = Modifier
-//          .background(Color.LightGray)
-//          .widthIn(0.dp, 232.dp),
-//      ) {
-//        Row(
-//          modifier = Modifier
-//            .fillMaxWidth(),
-//          horizontalArrangement = Arrangement.SpaceBetween,
-//        ) {
-//          Text("start line $index")
-//          Text("end line $index")
-//        }
-//      }
     }
   }
-
 }
 
 @Preview
