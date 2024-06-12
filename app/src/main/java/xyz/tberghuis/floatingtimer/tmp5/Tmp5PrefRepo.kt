c4de978b13ad88f8892c49e8e043a5219539b85c
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-06-12 11:48:40 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5PrefRepo.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5PrefRepo.kt
@@ -1,21 +1,24 @@
 package xyz.tberghuis.floatingtimer.tmp5
 
+import android.media.RingtoneManager
+import android.net.Uri
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import java.net.URI
 
 class Tmp5PrefRepo(
   private val dataStore: DataStore<Preferences>
 ) {
   val alarmRingtoneUriFlow: Flow<String?> = dataStore.data.map { preferences ->
-    // todo get default alarm uri
-    // if null
-    // use "" empty string to represent none.
-    preferences[stringPreferencesKey("alarm_ringtone_uri")]
-      ?: "content://settings/system/alarm_alert"
+    var uri = preferences[stringPreferencesKey("alarm_ringtone_uri")]
+    if (uri == null) {
+      uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)?.toString()
+    }
+    uri
   }
 
   suspend fun updateAlarmRingtoneUri(uri: String) {
---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp7/TmpPreferencesRepository.kt
@@ -0,0 +1,86 @@
+package xyz.tberghuis.floatingtimer.tmp7
+
+import androidx.compose.ui.graphics.Color
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.floatPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import xyz.tberghuis.floatingtimer.BuildConfig
+import xyz.tberghuis.floatingtimer.DEFAULT_HALO_COLOR
+import xyz.tberghuis.floatingtimer.logd
+
+class TmpPreferencesRepository(private val dataStore: DataStore<Preferences>) {
+  val vibrationFlow: Flow<Boolean> = dataStore.data.map { preferences ->
+    preferences[booleanPreferencesKey("vibration")] ?: true
+  }
+
+  suspend fun updateVibration(vibration: Boolean) {
+    dataStore.edit { preferences ->
+      preferences[booleanPreferencesKey("vibration")] = vibration
+    }
+  }
+
+  val soundFlow: Flow<Boolean> = dataStore.data.map { preferences ->
+    preferences[booleanPreferencesKey("sound")] ?: true
+  }
+
+  suspend fun updateSound(sound: Boolean) {
+    dataStore.edit { preferences ->
+      preferences[booleanPreferencesKey("sound")] = sound
+    }
+  }
+
+  val haloColourPurchasedFlow: Flow<Boolean> = dataStore.data.map { preferences ->
+    preferences[booleanPreferencesKey("halo_colour_purchased")] ?: BuildConfig.DEFAULT_PURCHASED
+  }
+
+  suspend fun updateHaloColourPurchased(purchased: Boolean) {
+    dataStore.edit { preferences ->
+      preferences[booleanPreferencesKey("halo_colour_purchased")] = purchased
+    }
+  }
+
+  val haloColourFlow: Flow<Color> = dataStore.data.map { preferences ->
+    val haloColourString = preferences[stringPreferencesKey("halo_colour")]
+    val haloColor = if (haloColourString == null)
+      DEFAULT_HALO_COLOR
+    else
+      Color(haloColourString.toULong())
+    logd("haloColourFlow halocolor $haloColor")
+    haloColor
+  }
+
+  suspend fun updateHaloColour(color: Color) {
+    logd("updateHaloColour")
+    val haloColourString = color.value.toString()
+    dataStore.edit { preferences ->
+      preferences[stringPreferencesKey("halo_colour")] = haloColourString
+    }
+  }
+
+  suspend fun resetHaloColour() {
+    dataStore.edit { preferences ->
+      preferences.remove(stringPreferencesKey("halo_colour"))
+    }
+  }
+
+  val bubbleScaleFlow: Flow<Float> = dataStore.data.map { preferences ->
+    preferences[floatPreferencesKey("bubble_scale")] ?: 0f
+  }
+
+  suspend fun updateBubbleScale(scale: Float) {
+    dataStore.edit { preferences ->
+      preferences[floatPreferencesKey("bubble_scale")] = scale
+    }
+  }
+
+  suspend fun resetBubbleScale() {
+    dataStore.edit { preferences ->
+      preferences.remove(floatPreferencesKey("bubble_scale"))
+    }
+  }
+}
