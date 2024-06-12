da68140e4b068b627037882a40364868a9d8eb2b
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-06-12 13:29:19 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp5/Tmp5RingtoneVm.kt
@@ -5,10 +5,11 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import xyz.tberghuis.floatingtimer.data.dataStore
+import xyz.tberghuis.floatingtimer.tmp7.TmpPreferencesRepository
 
 class Tmp5RingtoneVm(private val application: Application) : AndroidViewModel(application) {
   // todo get singleton from MainApplication
-  private val prefRepo = Tmp5PrefRepo(application.dataStore)
+  private val prefRepo = TmpPreferencesRepository(application.dataStore)
   val ringtonePreviewVmc = TmpRingtonePreviewVmc(application)
   val currentRingtoneVmc =
     TmpCurrentRingtoneVmc(prefRepo.alarmRingtoneUriFlow, viewModelScope, application)
