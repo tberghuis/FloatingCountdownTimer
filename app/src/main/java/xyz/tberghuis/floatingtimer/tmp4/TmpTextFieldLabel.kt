5dc3b37e8cda3724fbea5ad87a6a4c74443176d5
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-06-29 09:23:44 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpTextFieldLabel.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpTextFieldLabel.kt
@@ -1,51 +1,34 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
-import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.dp
 import xyz.tberghuis.floatingtimer.R
 
 
 @Composable
 fun TmpTextFieldLabel() {
-
-  val labelTfvState = remember {
-    mutableStateOf(TextFieldValue(""))
-  }
-
-  TextField(
-    value = labelTfvState.value,
-    onValueChange = {
-      labelTfvState.value = it
+  OutlinedTextField(
+    value = "this is a long stringthis is a long stringthis is a long stringthis is a long string",
+    onValueChange = {},
+    modifier = Modifier,
+    enabled = false,
+    readOnly = true,
+    label = {
+      Text(stringResource(R.string.current))
     },
-    modifier = Modifier
-      .widthIn(max = 150.dp),
-    label = { Text(stringResource(R.string.label)) }
-  )
-
-
-
-
-  TextField(
-    value = labelTfvState.value,
-    onValueChange = {
-      labelTfvState.value = it
-    },
-    modifier = Modifier
-      .widthIn(max = 150.dp),
-    label = { Text(stringResource(R.string.label)) },
-
-
-    singleLine = true,
-
+    colors = OutlinedTextFieldDefaults.colors(
+      disabledTextColor = MaterialTheme.colorScheme.onSurface,
+      disabledContainerColor = Color.Transparent,
+      disabledBorderColor = MaterialTheme.colorScheme.outline,
+      disabledLabelColor = MaterialTheme.colorScheme.primary,
     )
+  )
 
 
 }
