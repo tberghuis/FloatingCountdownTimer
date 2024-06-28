d94028556452ab5ae8e8703e5cfc7e8bbc9e4afa
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-06-29 08:56:37 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/TmpScreen.kt
@@ -1,12 +1,19 @@
 package xyz.tberghuis.floatingtimer.tmp
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
 
 @Composable
 fun TmpScreen(
@@ -17,6 +24,20 @@ fun TmpScreen(
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
+    Text("hello tmp screen")
+  }
+}
 
+@Preview
+@Composable
+fun TmpScreenPreview() {
+  FloatingTimerTheme {
+    Surface(
+      modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White),
+    ) {
+      TmpScreen()
+    }
   }
 }
