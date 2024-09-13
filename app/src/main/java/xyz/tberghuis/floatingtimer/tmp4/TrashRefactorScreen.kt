f23e0fc3db4041771b4621d7f3188ef93b4247ed
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-09-13 12:21:59 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TrashRefactorScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TrashRefactorScreen.kt
@@ -1,9 +1,12 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
+import android.view.KeyEvent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import xyz.tberghuis.floatingtimer.logd
@@ -12,7 +15,19 @@ import xyz.tberghuis.floatingtimer.logd
 fun TrashRefactorScreen(
   vm: Tmp4Vm = viewModel()
 ) {
-  Column() {
+  Column(
+    modifier = Modifier.onPreviewKeyEvent {
+      logd("onPreviewKeyEvent $it")
+      if (it.nativeKeyEvent.action == KeyEvent.ACTION_UP) {
+        when (it.nativeKeyEvent.keyCode) {
+          KeyEvent.KEYCODE_1 -> {
+            logd("number 1 pressed")
+          }
+        }
+      }
+      true
+    },
+  ) {
     Text("TrashRefactorScreen")
     Button(onClick = {
       logd("${vm.willitblend}")
