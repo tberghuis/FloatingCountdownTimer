3ed694d706c316c474cd967772e3a5ab0f6c21c2
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-09-13 11:57:11 +1000

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/Tmp4Vm.kt
@@ -0,0 +1,7 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import androidx.lifecycle.ViewModel
+
+class Tmp4Vm : ViewModel() {
+  val willitblend = "will it blend"
+}
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TrashRefactorScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TrashRefactorScreen.kt
@@ -1,14 +1,24 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import xyz.tberghuis.floatingtimer.logd
 
 @Composable
-fun TrashRefactorScreen() {
+fun TrashRefactorScreen(
+  vm: Tmp4Vm = viewModel()
+) {
   Column() {
     Text("TrashRefactorScreen")
+    Button(onClick = {
+      logd("${vm.willitblend}")
+    }) {
+      Text("button")
+    }
   }
 }
 
