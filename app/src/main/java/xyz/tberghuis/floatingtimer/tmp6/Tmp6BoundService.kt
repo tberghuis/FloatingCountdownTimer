091516601c4a9d65267898168674849f5da441de
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-09-14 06:30:01 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/Tmp6BoundService.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/Tmp6BoundService.kt
@@ -6,66 +6,87 @@ import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.IBinder
+import android.util.Log
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.async
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import xyz.tberghuis.floatingtimer.logd
 import xyz.tberghuis.floatingtimer.tmp4.ServiceBinder
+
+fun tmplog(s: String) = Log.d("111", s)
+
 
 class Tmp6BoundService<T : Service>(
   private val application: Application,
   private val serviceClass: Class<T>
 ) {
-  private var boundServiceDeferred: Deferred<T>? = null
+//  private val service = MutableStateFlow<T?>(null)
+  private val service = MutableSharedFlow<T>()
+  var job: Job? = null
 
   private val serviceFlow = callbackFlow {
     val connection = object : ServiceConnection {
       override fun onServiceConnected(className: ComponentName, binder: IBinder) {
-        logd("onServiceConnected")
+        tmplog("onServiceConnected")
         trySend((binder as ServiceBinder<T>).getService())
       }
 
       override fun onServiceDisconnected(arg0: ComponentName) {
-        boundServiceDeferred = null
+        tmplog("onServiceDisconnected")
+//        trySend(null)
+        job?.cancel()
+        job = null
       }
 
       override fun onBindingDied(name: ComponentName?) {
         super.onBindingDied(name)
-        boundServiceDeferred = null
+        tmplog("onBindingDied")
+//        trySend(null)
+        job?.cancel()
+        job = null
       }
     }
     val intent = Intent(application, serviceClass)
     application.startForegroundService(intent)
     application.bindService(intent, connection, 0)
     awaitClose {
+      tmplog("awaitClose")
       application.unbindService(connection)
     }
   }
 
-
   init {
-    logd("BoundService init")
+    tmplog("BoundService init")
+//    restartJob()
+  }
+
+  fun restartJob() {
+//    job?.cancel()
+    job = CoroutineScope(IO).launch {
+      serviceFlow.collect {
+        service.emit(it)
+      }
+    }
   }
 
   // doitwrong
   // do not keep reference to T:Service anywhere
   // always use provide function
   suspend fun provideService(/* refresh bool, manual invoke by user */): T {
-    logd("provideService")
-    boundServiceDeferred?.let {
-      return it.await()
+    tmplog("provideService")
+
+    if(job == null){
+      restartJob()
     }
 
-    return CoroutineScope(IO).async {
-      serviceFlow.first()
-    }.also {
-      boundServiceDeferred = it
-    }.await()
+    return service.first()
   }
 }
