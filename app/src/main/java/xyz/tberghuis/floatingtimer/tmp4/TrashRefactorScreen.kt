3252859ace33fc9d7144bce014e099b98b485056
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-09-13 11:53:34 +1000

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TrashRefactorScreen.kt
@@ -0,0 +1,19 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TrashRefactorScreen() {
+  Column() {
+    Text("TrashRefactorScreen")
+  }
+}
+
+@Preview
+@Composable
+fun TrashRefactorScreenPreview() {
+  TrashRefactorScreen()
+}
