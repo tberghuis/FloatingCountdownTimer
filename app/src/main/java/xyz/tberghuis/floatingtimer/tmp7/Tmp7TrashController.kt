1a2aa753ffa7989414dde4f2f6bc495519088a5e
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-09-16 12:11:50 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp7/Tmp7TrashController.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp7/Tmp7TrashController.kt
@@ -2,18 +2,22 @@ package xyz.tberghuis.floatingtimer.tmp7
 
 import android.graphics.PixelFormat
 import android.util.Log
+import android.view.Gravity
 import android.view.WindowManager
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import xyz.tberghuis.floatingtimer.TRASH_SIZE_DP
 import xyz.tberghuis.floatingtimer.composables.LocalFloatingService
 import xyz.tberghuis.floatingtimer.service.Bubble
 import xyz.tberghuis.floatingtimer.service.FloatingService
 import xyz.tberghuis.floatingtimer.service.TrashOverlay
 import xyz.tberghuis.floatingtimer.service.createComposeView
+import xyz.tberghuis.floatingtimer.service.dimensionDpToPx
 
 class TrashController(
   private val windowManager: WindowManager,
@@ -25,13 +29,21 @@ class TrashController(
 //  var isBubbleHoveringTrash = false
 
   private var overlay: ComposeView? = null
-  private val params: WindowManager.LayoutParams = WindowManager.LayoutParams(
-    WindowManager.LayoutParams.MATCH_PARENT,
-    WindowManager.LayoutParams.MATCH_PARENT,
+
+  private val trashSizePx =
+    dimensionDpToPx(TRASH_SIZE_DP.dp, service.resources.displayMetrics.density)
+
+  private val params = WindowManager.LayoutParams(
+    trashSizePx,
+    trashSizePx,
+    0,
+    100,
     WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
     WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
     PixelFormat.TRANSLUCENT
-  )
+  ).apply {
+    gravity = Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM
+  }
 
   init {
     service.scope.launch(Dispatchers.Main) {
@@ -67,7 +79,8 @@ class TrashController(
     val view = createComposeView(service)
     view.setContent {
       CompositionLocalProvider(LocalFloatingService provides service) {
-        TrashOverlay()
+//        TrashOverlay()
+        Trash()
       }
     }
     return view
