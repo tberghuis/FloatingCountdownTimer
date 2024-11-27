02b85e74c578d755aeb2246a10c755f98c969666
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-11-27 11:52:32 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpFtAlarmController.kt
@@ -0,0 +1,88 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import xyz.tberghuis.floatingtimer.providePreferencesRepository
+import xyz.tberghuis.floatingtimer.service.FloatingService
+import xyz.tberghuis.floatingtimer.service.countdown.Countdown
+
+class TmpFtAlarmController(
+  floatingService: FloatingService
+) {
+  private var ringtone: Ringtone? = null
+  private val finishedCountdowns = MutableStateFlow(setOf<Countdown>())
+
+  // watch ringtone uri to update
+
+  // looping : Boolean?
+  // vibrate : Boolean?
+  // ringtoneDuration : Long? = null
+
+  init {
+    val prefs = floatingService.application.providePreferencesRepository()
+    floatingService.scope.launch {
+      prefs.alarmRingtoneUriFlow.collect { uri ->
+        uri?.let {
+          ringtone?.stop()
+          ringtone =
+            RingtoneManager.getRingtone(floatingService.application, Uri.parse(uri))?.apply {
+              audioAttributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .setLegacyStreamType(AudioManager.STREAM_ALARM)
+                .build()
+              if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                isHapticGeneratorEnabled = false
+              }
+              if (Build.VERSION.SDK_INT >= 28) {
+                isLooping = true
+              }
+            }
+        }
+      }
+    }
+
+    floatingService.scope.launch {
+      try {
+        finishedCountdowns.collect {
+          withContext(Main.immediate) {
+            when (it.size) {
+              0 -> {
+                if (ringtone?.isPlaying == true) {
+                  ringtone?.stop()
+                }
+              }
+              // allow for single play ringtone on older apilevels < 28
+              else -> {
+                if (ringtone?.isPlaying == false) {
+                  ringtone?.play()
+                }
+              }
+            }
+          }
+        }
+      } finally {
+        // scope is cancelled
+        // lesson, don't call withContext in finally block
+        if (ringtone?.isPlaying == true) {
+          ringtone?.stop()
+        }
+      }
+    }
+  }
+
+  fun startAlarm(c: Countdown) {
+    finishedCountdowns.value += c
+  }
+
+  fun stopAlarm(c: Countdown) {
+    finishedCountdowns.value -= c
+  }
+}
