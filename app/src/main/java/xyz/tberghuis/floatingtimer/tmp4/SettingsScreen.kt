6186467c941ec4e92a78af7a3fc49910cd1ea33a
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-11-28 14:01:47 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/SettingsScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/SettingsScreen.kt
@@ -1,9 +1,57 @@
 package xyz.tberghuis.floatingtimer.tmp4
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import xyz.tberghuis.floatingtimer.LocalNavController
+import xyz.tberghuis.floatingtimer.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen() {
-  Text("hello settings screen")
+  val navController = LocalNavController.current
+  Scaffold(
+    modifier = Modifier,
+    topBar = {
+      TopAppBar(
+        title = { Text(stringResource(R.string.settings)) },
+        navigationIcon = {
+          IconButton(onClick = {
+            navController.navigateUp()
+          }) {
+            Icon(Icons.AutoMirrored.Filled.ArrowBack, stringResource(R.string.back))
+          }
+        },
+        modifier = Modifier,
+      )
+    },
+    snackbarHost = {},
+  ) { padding ->
+    SettingsScreenContent(padding)
+  }
+}
+
+@Composable
+fun SettingsScreenContent(padding: PaddingValues) {
+  Column(
+    modifier = Modifier
+      .padding(padding)
+      .fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+  ) {
+    Text("hello settings screen")
+  }
 }
