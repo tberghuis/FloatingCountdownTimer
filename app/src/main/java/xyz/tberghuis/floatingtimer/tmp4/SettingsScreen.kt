394cc125bf8dacfa9b63a9a6081d75a4440f1e56
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-11-30 09:15:37 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/SettingsScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/SettingsScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
@@ -102,6 +103,28 @@ fun SettingsScreenContent(
             vm.updateLooping(it)
           },
         )
+      },
+    )
+
+
+    HorizontalDivider()
+
+    Text(
+      "Timer",
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 16.dp)
+        .padding(horizontal = 16.dp),
+      color = MaterialTheme.colorScheme.primary,
+    )
+    ListItem(
+      headlineContent = { Text("Default color") },
+      modifier = Modifier.clickable {
+        navController.navigate("change_color")
+      },
+      supportingContent = { Text("todo") },
+      trailingContent = {
+        // todo color circle
       },
     )
 
