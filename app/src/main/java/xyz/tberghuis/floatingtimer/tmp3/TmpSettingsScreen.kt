27d5060485eaeb1a9a39ae4a73a88cf7b519b826
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-11-30 12:49:22 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpSettingsScreen.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp3/TmpSettingsScreen.kt
@@ -1,10 +1,5 @@
 package xyz.tberghuis.floatingtimer.tmp3
 
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import android.os.PowerManager
-import android.provider.Settings
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,18 +15,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.content.getSystemService
 import androidx.navigation.NavHostController
 import xyz.tberghuis.floatingtimer.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TmpSettingsScreen(nav: NavHostController) {
-
-
   Scaffold(
     modifier = Modifier,
     topBar = {
@@ -52,15 +42,9 @@ fun TmpSettingsScreen(nav: NavHostController) {
       verticalArrangement = Arrangement.Top,
       horizontalAlignment = Alignment.Start,
     ) {
-      DefaultColor(nav)
-      DefaultSize(nav)
-
-      Text("change system alarm ringtone")
       Text("notification settings")
       Text("draw overlay setting")
-
       BatterySettings()
-
       Text("dontkillmyapp.com")
       Text("support (github new issue)")
       Text("app version")
@@ -69,34 +53,11 @@ fun TmpSettingsScreen(nav: NavHostController) {
 }
 
 @Composable
-fun DefaultColor(nav: NavHostController) {
-  Text(
-    "Default Color",
-    modifier = Modifier.clickable {
-      nav.navigate("change_color/default")
-    },
-  )
-}
-
-@Composable
-fun DefaultSize(nav: NavHostController) {
-  Text(
-    "Default Size",
-    modifier = Modifier.clickable {
-      nav.navigate("change_size/default")
-    },
-  )
-}
-
-
-@Composable
 fun BatterySettings() {
 //  val context = LocalContext.current
-
   Text(
     "battery settings",
     modifier = Modifier.clickable {
-
 //                                  i heard stories of being banned for having this
       // ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
 
@@ -109,8 +70,6 @@ fun BatterySettings() {
 //        intent.data = Uri.parse("package:${context.packageName}")
 //      }
 //      context.startActivity(intent)
-
-
     },
   )
 }
