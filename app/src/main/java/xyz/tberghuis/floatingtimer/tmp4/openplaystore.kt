9cb6c38429de9270e8c25d92a366c8a7e33ab374
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-12-17 15:23:36 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/openplaystore.kt
@@ -0,0 +1,53 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+
+fun Context.openPlayStorePage() {
+  val intent = Intent(Intent.ACTION_VIEW)
+    .setData(Uri.parse("https://play.google.com/store/apps/details?id=$packageName"))
+  try {
+    startActivity(
+      Intent(intent)
+        .setPackage("com.android.vending")
+    )
+  } catch (exception: ActivityNotFoundException) {
+    startActivity(intent)
+  }
+}
+
+fun Context.openGithubIssues() {
+  val intent = Intent(Intent.ACTION_VIEW)
+    .setData(Uri.parse("https://github.com/tberghuis/FloatingCountdownTimer/issues"))
+  startActivity(intent)
+}
+
+@Preview
+@Composable
+fun OpenPlayPage() {
+  val context = LocalContext.current
+  Column {
+    Button(onClick = {
+      context.openPlayStorePage()
+    }) {
+      Text("play store")
+    }
+
+    Button(onClick = {
+      context.openGithubIssues()
+    }) {
+      Text("github issues")
+    }
+  }
+}
+
+
+
