b1f79a79ba53694048aad1c24d76f60a0e9bc20f
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-01-15 15:09:53 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/util/uriToRingtone.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/util/uriToRingtone.kt
@@ -5,12 +5,6 @@ import android.media.Ringtone
 import android.media.RingtoneManager
 import android.net.Uri
 
-// todo rename file uriToRingtone.kt
-
 fun uriToRingtone(context: Context, uri: Uri): Ringtone? {
   return RingtoneManager.getRingtone(context, uri)
 }
-//
-//fun String.toUri(): Uri {
-//  return Uri.parse(this)
-//}
