96730b447e20f01b1ba2dd80bf5ff607fd67edac
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-01-25 09:32:20 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpCountdownCircleView.kt
@@ -0,0 +1,41 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import xyz.tberghuis.floatingtimer.service.countdown.CountdownProgressArc
+
+@Composable
+fun TmpCountdownCircleView(
+  timeLeftFraction: Float,
+  countdownSeconds: Int,
+) {
+  SquareBackground(
+    modifier = Modifier.padding(8.dp / 2), // arcWidth / 2
+    background = {
+      CountdownProgressArc(
+        timeLeftFraction,
+        8.dp,
+        Color.Blue,
+        false
+      )
+    },
+  ) {
+    Box(Modifier.padding(5.dp)) {
+//      TimeDisplay(countdownSeconds)
+      // todo
+      Text("time goes here")
+    }
+  }
+}
+
+@Preview
+@Composable
+fun CountdownCircleViewPreview() {
+  TmpCountdownCircleView(.5f, 3600 + 59)
+}
