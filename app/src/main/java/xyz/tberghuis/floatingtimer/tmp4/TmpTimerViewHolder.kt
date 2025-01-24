08521e59446c0fe3b470c1984e70b5f7b74b751f
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-01-25 09:21:55 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/TmpTimerViewHolder.kt
@@ -0,0 +1,38 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import android.graphics.PixelFormat
+import android.view.Gravity
+import android.view.WindowManager
+import xyz.tberghuis.floatingtimer.logd
+import xyz.tberghuis.floatingtimer.service.FloatingService
+import xyz.tberghuis.floatingtimer.service.createComposeView
+
+class TmpTimerViewHolder(
+  val service: FloatingService,
+  x: Int? = null,
+  y: Int? = null
+) {
+  val params = WindowManager.LayoutParams()
+  val view = createComposeView(service)
+
+  init {
+    initParams(x, y)
+    logd("TimerViewHolder params $params")
+  }
+
+  private fun initParams(
+    x: Int?,
+    y: Int?
+  ) {
+    params.x = x ?: 0
+    params.y = y ?: 0
+    params.width = WindowManager.LayoutParams.WRAP_CONTENT
+    params.height = WindowManager.LayoutParams.WRAP_CONTENT
+    params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+    params.flags =
+      WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+    params.format = PixelFormat.TRANSLUCENT
+    params.gravity = Gravity.TOP or Gravity.LEFT
+    logd("params x y ${params.x} ${params.y}")
+  }
+}
