ea63fb66632eca436e128a04ab8de52ebe64896a
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-01-25 09:26:35 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp4/SquareBackground.kt
@@ -0,0 +1,48 @@
+package xyz.tberghuis.floatingtimer.tmp4
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.unit.Constraints
+import kotlin.math.max
+
+// assumes content and background contains a single composable
+// use Modifier.fillMaxSize() in background composable
+@Composable
+fun SquareBackground(
+  modifier: Modifier = Modifier,
+  background: @Composable () -> Unit,
+  content: @Composable () -> Unit
+) {
+  SubcomposeLayout(
+    modifier
+  ) { constraints ->
+    val contentMeasurable = subcompose("content", content)[0]
+    val contentPlaceable = contentMeasurable.measure(constraints)
+//    val sideLength = max(contentPlaceable.width, contentPlaceable.height)
+    val sideLength = max(contentPlaceable.measuredWidth, contentPlaceable.measuredHeight)
+    layout(sideLength, sideLength) {
+      val backgroundMeasurable = subcompose("background") {
+        Box(
+          modifier = Modifier.size(sideLength.toDp()),
+        ) {
+          background()
+        }
+      }.first()
+      val backgroundConstraints = Constraints(
+        minWidth = sideLength,
+        maxWidth = sideLength,
+        minHeight = sideLength,
+        maxHeight = sideLength
+      )
+      val backgroundPlaceable = backgroundMeasurable.measure(backgroundConstraints)
+      backgroundPlaceable.place(0, 0)
+      contentPlaceable.placeRelative(
+        (sideLength - contentPlaceable.width) / 2,
+        (sideLength - contentPlaceable.height) / 2
+      )
+    }
+  }
+}
