3c413dc13587f18a8e2206866ebd40c7a9f23ba3
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-01-26 09:12:38 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/CountdownInputRow.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp6/CountdownInputRow.kt
@@ -1,5 +1,6 @@
 package xyz.tberghuis.floatingtimer.tmp6
 
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,8 +15,10 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -85,7 +88,10 @@ fun CountdownInputRowPreview() {
 
 
   Box(
-
+    modifier = Modifier
+      .background(Color.Blue.copy(alpha=.2f))
+      .fillMaxWidth(.8f),
+    contentAlignment = Alignment.TopStart,
   ) {
     CountdownInputRow(hours, minutes, seconds)
   }
