1db270126a3b8c03e7cbbdd404497968bfaa3a0d
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-01-26 15:17:11 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/service/stopwatch/StopwatchCircleView.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/service/stopwatch/StopwatchCircleView.kt
@@ -16,6 +16,22 @@ import androidx.compose.ui.unit.TextUnit
 import kotlinx.coroutines.flow.MutableStateFlow
 import xyz.tberghuis.floatingtimer.composables.TimeDisplay
 import xyz.tberghuis.floatingtimer.tmp4.SquareBackground
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.geometry.Size
+import kotlinx.coroutines.flow.StateFlow
 
 @Composable
 fun StopwatchCircleView(
@@ -57,6 +73,80 @@ fun StopwatchCircleView(
       contentAlignment = Alignment.Center,
     ) {
       TimeDisplay(timeElapsed, fontSize, isBackgroundTransparent)
+    }
+  }
+}
+
+@Composable
+fun StopwatchBorderArc(
+  isRunningStateFlow: StateFlow<Boolean>?,
+  arcWidth: Dp,
+  haloColor: Color,
+  isBackgroundTransparent: Boolean
+) {
+  var pausedAngle by remember { mutableFloatStateOf(210f) }
+  var restartAngle by remember { mutableFloatStateOf(0f) }
+  val infiniteTransition = rememberInfiniteTransition()
+
+  val animatedAngle by infiniteTransition.animateFloat(
+    initialValue = 0f, targetValue = 360f, animationSpec = infiniteRepeatable(
+      animation = tween(3000, easing = LinearEasing), repeatMode = RepeatMode.Restart
+    )
+  )
+  val drawAnimatedAngle by remember {
+    derivedStateOf { pausedAngle + animatedAngle - restartAngle }
+  }
+
+  val running = isRunningStateFlow?.collectAsState()?.value ?: false
+
+  Canvas(
+    Modifier.fillMaxSize()
+  ) {
+    if (!isBackgroundTransparent) {
+      drawCircle(
+        color = Color.White,
+      )
+      drawArc(
+        color = Color.White,
+        startAngle = 0f,
+        sweepAngle = 360f,
+        useCenter = false,
+        style = Stroke(arcWidth.toPx()),
+        size = Size(size.width, size.height)
+      )
+    }
+
+    drawArc(
+      color = haloColor.copy(alpha = .1f),
+      startAngle = 0f,
+      sweepAngle = 360f,
+      useCenter = false,
+      style = Stroke(arcWidth.toPx()),
+      size = Size(size.width, size.height)
+    )
+
+    drawArc(
+      color = haloColor,
+      startAngle = if (!running) pausedAngle else drawAnimatedAngle,
+      sweepAngle = 120f,
+      useCenter = false,
+      style = Stroke(arcWidth.toPx()),
+      size = Size(size.width, size.height)
+    )
+  }
+
+  // should learn to write my own delegate for angle instead
+  LaunchedEffect(Unit) {
+    isRunningStateFlow?.collect {
+      when (it) {
+        true -> {
+          restartAngle = animatedAngle
+        }
+
+        false -> {
+          pausedAngle = drawAnimatedAngle
+        }
+      }
     }
   }
 }
