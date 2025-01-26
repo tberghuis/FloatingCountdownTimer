049dbaa0ef4b9089f3505a7df4fdbaff63e317ba
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-01-26 14:58:40 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/service/countdown/CountdownView.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/service/countdown/CountdownView.kt
@@ -1,15 +1,10 @@
 package xyz.tberghuis.floatingtimer.service.countdown
 
-import android.content.Context
-import android.view.WindowManager
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.unit.IntSize
-import xyz.tberghuis.floatingtimer.composables.LocalTimerViewHolder
-import xyz.tberghuis.floatingtimer.logd
 import xyz.tberghuis.floatingtimer.tmp5.TmpCountdownView
 import xyz.tberghuis.floatingtimer.tmp4.TmpBubbleProperties as BubbleProperties
 
@@ -39,52 +34,3 @@ fun CountdownView(
     isPaused
   )
 }
-
-//@Composable
-//fun XxxCountdownView(
-//  bubbleProperties: BubbleProperties,
-//  timeLeftFraction: Float,
-//  countdownSeconds: Int,
-//  isPaused: Boolean
-//) {
-//  when (bubbleProperties.timerShape) {
-//    "circle" -> {
-//      CountdownCircleView(
-//        bubbleProperties = bubbleProperties,
-//        timeLeftFraction = timeLeftFraction,
-//        countdownSeconds = countdownSeconds,
-//        isPaused = isPaused,
-//        bubbleProperties.isBackgroundTransparent
-//      )
-//    }
-//
-//    "label", "rectangle" -> {
-//      val tvh = LocalTimerViewHolder.current
-//      val updateViewLayout = tvh?.let {
-//        { size: IntSize ->
-//          logd("runOnceOnGloballyPositioned $size")
-//          tvh.params.width = size.width
-//          tvh.params.height = size.height
-//          tvh.service.ftWindowManager.updateViewLayout(tvh.view, tvh.params)
-//        }
-//      }
-//      // this is redundant, unless bad data in DB
-//      val label = if (bubbleProperties.timerShape == "label") bubbleProperties.label else null
-//      TimerRectView(
-//        isPaused,
-//        bubbleProperties.arcWidth,
-//        bubbleProperties.haloColor,
-//        countdownSeconds,
-//        timeLeftFraction,
-//        bubbleProperties.fontSize,
-//        label,
-//        bubbleProperties.isBackgroundTransparent,
-//        updateViewLayout
-//      )
-//    }
-//
-//    else -> {
-//      throw RuntimeException("invalid timer shape")
-//    }
-//  }
-//}
