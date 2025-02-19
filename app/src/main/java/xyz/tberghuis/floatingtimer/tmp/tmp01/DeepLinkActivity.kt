c70c4f8c8de8583635ccca64b86fd6080a79d7c7
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-19 11:59:01 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp01/DeepLinkActivity.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp01/DeepLinkActivity.kt
@@ -4,14 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
 
 class DeepLinkActivity : ComponentActivity() {
@@ -20,15 +12,7 @@ class DeepLinkActivity : ComponentActivity() {
     enableEdgeToEdge()
     setContent {
       FloatingTimerTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Column(
-            modifier = Modifier.padding(innerPadding),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start,
-          ) {
-            Text("hello deeplink")
-          }
-        }
+        DeepLinkScreen()
       }
     }
   }
---
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp01/DeepLinkScreen.kt
@@ -0,0 +1,24 @@
+package xyz.tberghuis.floatingtimer.tmp.tmp01
+
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DeepLinkScreen() {
+  Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    Column(
+      modifier = Modifier.padding(innerPadding),
+      verticalArrangement = Arrangement.Top,
+      horizontalAlignment = Alignment.Start,
+    ) {
+      Text("hello deeplink")
+    }
+  }
+}
