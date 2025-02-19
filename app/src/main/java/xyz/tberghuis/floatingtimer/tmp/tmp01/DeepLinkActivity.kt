d1989dcd44cb6bd99cb941edde50b70dd57f8b81
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-19 12:15:45 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp01/DeepLinkActivity.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp01/DeepLinkActivity.kt
@@ -2,8 +2,11 @@ package xyz.tberghuis.floatingtimer.tmp.tmp01
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
 
 class DeepLinkActivity : ComponentActivity() {
@@ -11,6 +14,15 @@ class DeepLinkActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
+      val vm: DeepLinkScreenVm = viewModel()
+      val activity = LocalActivity.current
+      // hack to only run once even if configuration change
+      rememberSaveable {
+        activity?.intent?.data?.let {
+          vm.processDataUri(it)
+        }
+        ""
+      }
       FloatingTimerTheme {
         DeepLinkScreen()
       }
--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp01/DeepLinkScreenVm.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp01/DeepLinkScreenVm.kt
@@ -1,13 +1,21 @@
 package xyz.tberghuis.floatingtimer.tmp.tmp01
 
 import android.app.Application
+import android.net.Uri
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
+import xyz.tberghuis.floatingtimer.logd
 
 class DeepLinkScreenVm(
   private val application: Application,
 ) : AndroidViewModel(application) {
   var link by mutableStateOf("")
+
+  fun processDataUri(data: Uri) {
+    logd("data uri $data")
+    link = data.toString()
+  }
+
 }
