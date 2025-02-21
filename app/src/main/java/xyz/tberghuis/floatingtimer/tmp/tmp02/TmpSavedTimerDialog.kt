e183ace23a3f86d1bde65f8e5ee1eb548cc69766
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-21 13:45:13 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/TmpSavedTimerDialog.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/TmpSavedTimerDialog.kt
@@ -3,7 +3,6 @@ package xyz.tberghuis.floatingtimer.tmp.tmp02
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -11,7 +10,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Link
 import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,7 +20,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -57,7 +54,6 @@ fun SavedTimerOptionsDialog(
           modifier = Modifier,
           horizontalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-
           IconButton(
             onClick = {
               vmc.showLinkDialog = vmc.showOptionsDialog
@@ -66,9 +62,6 @@ fun SavedTimerOptionsDialog(
           ) {
             Icon(Icons.Default.Link, "link")
           }
-
-
-
           IconButton(
             onClick = {
               vmc.deleteSavedTimer()
@@ -76,7 +69,6 @@ fun SavedTimerOptionsDialog(
           ) {
             Icon(Icons.Default.Delete, "delete")
           }
-
           TextButton(onClick = {
             vmc.showOptionsDialog = null
           }) {
