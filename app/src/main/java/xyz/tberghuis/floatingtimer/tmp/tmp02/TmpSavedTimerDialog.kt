25bf254086998247d903c6bb6c091553af81f806
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-21 13:14:02 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/TmpSavedTimerDialog.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/TmpSavedTimerDialog.kt
@@ -2,13 +2,17 @@ package xyz.tberghuis.floatingtimer.tmp.tmp02
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,8 +29,14 @@ fun SavedTimerOptionsDialog(
     },
     modifier = Modifier,
   ) {
-    Surface() {
-      Column {
+    Surface(
+      modifier = Modifier,
+      shape = MaterialTheme.shapes.large,
+      color = MaterialTheme.colorScheme.surfaceVariant,
+    ) {
+      Column(
+        modifier = Modifier.padding(16.dp),
+      ) {
         Text("Saved Timer Options")
         Row {
           Button(onClick = {
