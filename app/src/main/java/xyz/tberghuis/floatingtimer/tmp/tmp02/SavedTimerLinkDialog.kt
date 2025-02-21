fd8a3f54c72f3fa271063e55d1a69093608c9958
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-21 13:51:01 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/SavedTimerLinkDialog.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/SavedTimerLinkDialog.kt
@@ -2,10 +2,13 @@ package xyz.tberghuis.floatingtimer.tmp.tmp02
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,7 +34,13 @@ fun SavedTimerLinkDialog(
     },
     modifier = Modifier,
   ) {
-    Surface() {
+    Surface(
+      modifier = Modifier
+        .wrapContentWidth()
+        .wrapContentHeight(),
+      shape = MaterialTheme.shapes.large,
+      color = MaterialTheme.colorScheme.surfaceVariant,
+    ) {
       Column {
         Text("Timer Link")
         Row(
