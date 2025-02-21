9506adaa5809059ab9999094bfa3c2dce6ac60e4
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-22 09:29:23 +1100

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "xyz.tberghuis.floatingtimer"
     minSdk = 26
     targetSdk = 34
-    versionCode = 78
-    versionName = "1.42.0"
+    versionCode = 79
+    versionName = "1.43.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
