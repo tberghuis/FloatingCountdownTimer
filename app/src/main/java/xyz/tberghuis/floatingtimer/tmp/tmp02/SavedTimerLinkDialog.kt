7268a9b48a2b1a97887b2cb380a60bcf69d7eb84
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-21 13:57:19 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/SavedTimerLinkDialog.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/SavedTimerLinkDialog.kt
@@ -7,12 +7,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,10 +27,7 @@ fun SavedTimerLinkDialog(
   if (vmc.showLinkDialog == null) {
     return
   }
-
   val clipboardManager = LocalClipboardManager.current
-
-
   BasicAlertDialog(
     onDismissRequest = {
       vmc.showLinkDialog = null
@@ -59,12 +56,14 @@ fun SavedTimerLinkDialog(
           )
           Text("auto start")
         }
-
-        Row {
-          Button(onClick = { vmc.deepLinkToClipboard(clipboardManager) }) {
+        Row(
+          modifier = Modifier,
+          horizontalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+          TextButton(onClick = { vmc.deepLinkToClipboard(clipboardManager) }) {
             Text("clipboard")
           }
-          Button(onClick = {
+          TextButton(onClick = {
             vmc.showLinkDialog = null
           }) {
             Text("cancel")
