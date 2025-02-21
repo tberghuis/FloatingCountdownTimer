b446c81a05eda456533f7248caa53c1824feb977
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-21 13:20:06 +1100

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/TmpSavedTimerDialog.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/TmpSavedTimerDialog.kt
@@ -3,12 +3,18 @@ package xyz.tberghuis.floatingtimer.tmp.tmp02
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Link
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
@@ -38,17 +44,30 @@ fun SavedTimerOptionsDialog(
         modifier = Modifier.padding(16.dp),
       ) {
         Text("Saved Timer Options")
-        Row {
-          Button(onClick = {
-            vmc.showLinkDialog = vmc.showOptionsDialog
-            vmc.showOptionsDialog = null
-          }) {
-            Text("link")
+        Row(
+
+        ) {
+
+          IconButton(
+            onClick = {
+              vmc.showLinkDialog = vmc.showOptionsDialog
+              vmc.showOptionsDialog = null
+            },
+          ) {
+            Icon(Icons.Default.Link, "link")
           }
-          Button(onClick = { vmc.deleteSavedTimer() }) {
-            Text("delete")
+
+
+
+          IconButton(
+            onClick = {
+              vmc.deleteSavedTimer()
+            },
+          ) {
+            Icon(Icons.Default.Delete, "delete")
           }
-          Button(onClick = {
+
+          TextButton(onClick = {
             vmc.showOptionsDialog = null
           }) {
             Text("cancel")
