be4a3e6cfa917129322bebe3d27ba5507dded987
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-05-22 13:41:11 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/composables/FtBottomBar.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/composables/FtBottomBar.kt
@@ -1,8 +1,13 @@
 package xyz.tberghuis.floatingtimer.composables
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
@@ -10,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Timer
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +25,7 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import xyz.tberghuis.floatingtimer.LocalNavController
 import xyz.tberghuis.floatingtimer.R
@@ -42,42 +49,64 @@ fun FtBottomBar(currentScreen: ScreenType) {
     }
   }
 
-  NavigationBar() {
-    NavigationBarItem(
-      selected = currentScreen is ScreenTypeCountdown,
-      onClick = {
-        if (currentScreen is ScreenTypeCountdown) {
-          return@NavigationBarItem
-        }
-        navigateTo("countdown")
-      },
-      icon = {
-        Icon(Icons.Default.Timer, contentDescription = stringResource(R.string.countdown))
-      },
-      modifier = Modifier
-        .semantics { testTagsAsResourceId = true }
-        .testTag("NavCountdown"),
-      label = {
-        Text(text = stringResource(R.string.countdown))
-      },
-    )
-    NavigationBarItem(
-      selected = currentScreen is ScreenTypeStopwatch,
-      onClick = {
-        if (currentScreen is ScreenTypeStopwatch) {
-          return@NavigationBarItem
-        }
-        navigateTo("stopwatch")
-      },
-      icon = {
-        Icon(Icons.Default.Timer, contentDescription = stringResource(R.string.stopwatch))
-      },
-      modifier = Modifier
-        .semantics { testTagsAsResourceId = true }
-        .testTag("NavStopwatch"),
-      label = {
-        Text(text = stringResource(R.string.stopwatch))
-      },
-    )
+  NavigationBar(
+//    windowInsets = WindowInsets.navigationBars
+  ) {
+
+    Row(
+      Modifier.padding(
+//        bottom = WindowInsets.safeContent.asPaddingValues()
+//          .calculateBottomPadding() - WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+        bottom = 10.dp
+
+      )
+    ) {
+      NavigationBarItem(
+        selected = currentScreen is ScreenTypeCountdown,
+        onClick = {
+          if (currentScreen is ScreenTypeCountdown) {
+            return@NavigationBarItem
+          }
+          navigateTo("countdown")
+        },
+        icon = {
+          Icon(Icons.Default.Timer, contentDescription = stringResource(R.string.countdown))
+        },
+        modifier = Modifier
+          .semantics { testTagsAsResourceId = true }
+          .testTag("NavCountdown"),
+        label = {
+          Text(text = stringResource(R.string.countdown))
+        },
+      )
+      NavigationBarItem(
+        selected = currentScreen is ScreenTypeStopwatch,
+        onClick = {
+          if (currentScreen is ScreenTypeStopwatch) {
+            return@NavigationBarItem
+          }
+          navigateTo("stopwatch")
+        },
+        icon = {
+          Icon(Icons.Default.Timer, contentDescription = stringResource(R.string.stopwatch))
+        },
+        modifier = Modifier
+          .semantics { testTagsAsResourceId = true }
+          .testTag("NavStopwatch"),
+        label = {
+          Text(text = stringResource(R.string.stopwatch))
+        },
+      )
+
+    }
+
+
+  }
+}
+
+@Composable
+fun Xxx() {
+  Row {
+
   }
 }
