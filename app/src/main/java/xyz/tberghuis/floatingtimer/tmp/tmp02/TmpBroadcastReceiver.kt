581bd8d8451ea915ba4288bc3b2c39cef402ecb5
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-06-10 09:56:17 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/TmpBroadcastReceiver.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/TmpBroadcastReceiver.kt
@@ -4,12 +4,18 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import xyz.tberghuis.floatingtimer.logd
+import xyz.tberghuis.floatingtimer.service.FloatingService
 
 class TmpBroadcastReceiver : BroadcastReceiver() {
   override fun onReceive(context: Context, intent: Intent) {
-    if (intent.action == "com.example.snippets.ACTION_UPDATE_DATA") {
-      val data = intent.getStringExtra("com.example.snippets.DATA") ?: "No data"
-      logd("onReceive data $data")
-    }
+//    if (intent.action == "com.example.snippets.ACTION_UPDATE_DATA") {
+//      val data = intent.getStringExtra("com.example.snippets.DATA") ?: "No data"
+//      logd("onReceive data $data")
+//    }
+
+    logd("TmpBroadcastReceiver onReceive")
+    val intent = Intent(context, FloatingService::class.java)
+    context.startForegroundService(intent)
+
   }
 }
